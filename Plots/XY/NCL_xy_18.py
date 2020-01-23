def107dbba8f4da43b1c8ff601a68dd689a83789
Kevin Paul <kpaul@ucar.edu>
2020-01-23 10:18:37 -0700

    Adding initial xy_18 example file

---
+++ Plots/XY/NCL_xy_18.py
@@ -0,0 +1,33 @@
+"""
+NCL_xy_18.py
+============
+Concepts illustrated:
+
+- Filling the area between two curves in an XY plot
+- Labeling the bottom X axis with years
+- Drawing a main title on three separate lines
+- Calculating a weighted average
+- Changing the size/shape of an XY plot using viewport resources
+- Manually creating a legend
+- Overlaying XY plots on each other
+- Maximizing plots after they've been created
+
+See the [original NCL example](https://www.ncl.ucar.edu/Applications/Scripts/xy_18.ncl)
+"""
+
+###############################################################################
+# Open files and read in monthly data
+
+
+
+###############################################################################
+# Some parameters
+nyrs = 110
+nlon = 128
+nlat = 64
+
+###############################################################################
+# Note the inline comment in the previous code cell. This was not converted to
+# its own markdown cell because it was not contiguously preceeded with a
+# comment line beginning with 20+ '#' characters.
+
