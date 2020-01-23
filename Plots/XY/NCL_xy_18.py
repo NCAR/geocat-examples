404bfb6e1679c614637b97493fd2bd707b97e0cb
Kevin Paul <kpaul@ucar.edu>
2020-01-23 10:47:05 -0700

    Adding content

--- Plots/XY/NCL_xy_18.py
+++ Plots/XY/NCL_xy_18.py
@@ -16,8 +16,24 @@ See the [original NCL example](https://www.ncl.ucar.edu/Applications/Scripts/xy_
 """
 
 ###############################################################################
+# Basic imports
+import numpy as np
+import xarray as xr
+
+###############################################################################
 # Open files and read in monthly data
 
+pass
+#  dir  = './'
+#  v1	= addfile (dir+"TREFHT.B06.61.atm.1890-1999ANN.nc", "r")
+#  v2	= addfile (dir+"TREFHT.B06.59.atm.1890-1999ANN.nc", "r")
+#  v3	= addfile (dir+"TREFHT.B06.60.atm.1890-1999ANN.nc", "r")
+#  v4	= addfile (dir+"TREFHT.B06.57.atm.1890-1999ANN.nc", "r")
+#  n1	= addfile (dir+"TREFHT.B06.66.atm.1890-1999ANN.nc", "r")
+#  n2	= addfile (dir+"TREFHT.B06.67.atm.1890-1999ANN.nc", "r")
+#  n3	= addfile (dir+"TREFHT.B06.68.atm.1890-1999ANN.nc", "r")
+#  n4	= addfile (dir+"TREFHT.B06.69.atm.1890-1999ANN.nc", "r")
+#  g    = addfile (dir+"gw.nc","r")
 
 
 ###############################################################################
@@ -25,9 +41,14 @@ See the [original NCL example](https://www.ncl.ucar.edu/Applications/Scripts/xy_
 nyrs = 110
 nlon = 128
 nlat = 64
+time = np.linspace(1890, 1999, endpoint=True)
 
 ###############################################################################
-# Note the inline comment in the previous code cell. This was not converted to
-# its own markdown cell because it was not contiguously preceeded with a
-# comment line beginning with 20+ '#' characters.
+# OBS
 
+pass
+# obs = asciiread("jones_glob_ann_2002.asc",(/146/),"float")
+
+###############################################################################
+# NATURAL
+nat =
