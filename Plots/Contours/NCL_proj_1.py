d5d2c59a6a9dab196fdc3adbacd6f361cbef379a
Julia Kent <jkent@ucar.edu>
2020-01-23 15:14:06 -0700

    edit linewidths

--- Plots/Contours/NCL_proj_1.py
+++ Plots/Contours/NCL_proj_1.py
@@ -26,7 +26,7 @@ t = ds.TS.isel(time=0)
 fig = plt.figure(figsize=(10,10))
 
 ax = plt.axes(projection=ccrs.Mollweide())
-ax.coastlines()
+ax.coastlines(linewidths=0.5)
 
 gl = ax.gridlines(crs=ccrs.PlateCarree(),
                   linewidth=1, color='k', alpha=0.5)
@@ -35,7 +35,7 @@ a = t.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),
                     levels = 11, cmap = 'gist_rainbow_r', 
                     cbar_kwargs={"orientation": "horizontal", "label":''});
 a = t.plot.contour(ax=ax, transform=ccrs.PlateCarree(), 
-                   linewidths=0.5, cmap='k')
+                   levels = 11, linewidths=0.5, cmap='k')
 
 plt.suptitle('Example of a Mollweide Projection', y = .8, fontsize=18)
 plt.title('Surface Temperature                       K', fontsize=14)
