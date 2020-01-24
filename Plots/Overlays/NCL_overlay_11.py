8307611ba5d74a9296b281016ca942520c736741
Kevin Paul <kpaul@ucar.edu>
2020-01-24 12:56:06 -0700

    Intially add copy of template

---
+++ Plots/Overlays/NCL_overlay_11.py
@@ -0,0 +1,27 @@
+"""
+example_name.py
+===============
+Text description of the example
+
+Consider linking to the NCL documentation here
+"""
+
+###############################################################################
+# A line beginning with at least 20 '#' characters indicates that all of the
+# following comment lines should be interpreted as a markdown cell when the
+# script is converted to a Jupyter Notebook.
+print("This is a code cell")
+
+
+###############################################################################
+# Another text/markdown cell
+print("More code")
+# This line will be a comment inside a code cell, *NOT* a markdown cell
+print("Even more code")
+
+
+###############################################################################
+# Note the inline comment in the previous code cell. This was not converted to
+# its own markdown cell because it was not contiguously preceeded with a
+# comment line beginning with 20+ '#' characters.
+
