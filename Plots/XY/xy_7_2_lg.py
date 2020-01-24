2d5cea44c82c5b918c91e9c3cf6e2d8871f70e6d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-01-24 14:44:11 -0700

    Fix missing data point issue

--- Plots/XY/xy_7_2_lg.py
+++ Plots/XY/xy_7_2_lg.py
@@ -3,30 +3,31 @@ xy_7_2_lg.py
 ===============
 An example of a double y plot: two separate line with their own unique axis.
 
-https://www.ncl.ucar.edu/Applications/Images/xy_7_2_lg.png
+- Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/xy_7.ncl
+- https://www.ncl.ucar.edu/Applications/Images/xy_7_2_lg.png
 
 """
 
 ###############################################################################
 # Import modules
+# ===============
 
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+import geocat.datafiles
 
 ################################################################################
-#
-# open data file and extract a slice of the data
-#
+# Open data file and extract a slice of the data
+# ===============================================
 
-dset = xr.open_dataset("../../data/netcdf_files/TestData.xy3.nc")
-ds = dset.isel(case=0, time=slice(0, 35))
+dset = xr.open_dataset(geocat.datafiles.get("netcdf_files/TestData.xy3.nc"))
+ds = dset.isel(case=0, time=slice(0, 36))
 
 
 ################################################################################
-#
 # Create XY plot with two different Y axes
-#
+# =========================================
 
 
 def nclize_axis(ax):
@@ -37,7 +38,7 @@ def nclize_axis(ax):
 
     ax.tick_params(labelsize="small")
     ax.minorticks_on()
-    ax.xaxis.set_minor_locator(tic.AutoMinorLocator(n=3))
+    ax.xaxis.set_minor_locator(tic.AutoMinorLocator(n=5))
     ax.yaxis.set_minor_locator(tic.AutoMinorLocator(n=3))
 
     # length and width are in points and may need to change depending on figure size etc.
@@ -63,37 +64,31 @@ def nclize_axis(ax):
     )
 
 
-fig, ax1 = plt.subplots(figsize=(12, 12))
-
-fontsize = 24
-labelsize = 20
-color = "blue"
-linestyle = "-"
+fig, ax1 = plt.subplots(figsize=(12, 8))
 nclize_axis(ax1)
-ax1.set_xlabel(ds.time.long_name, fontsize=fontsize)
-ax1.set_ylabel(f"{ds.T.long_name} [solid]", fontsize=fontsize)
-ax1.plot(ds.time, ds.T, color=color, linestyle=linestyle, linewidth=2.0)
-ax1.tick_params(axis="both", labelsize=labelsize)
+ax1.set_xlabel(ds.time.long_name, fontsize=24)
+ax1.set_ylabel(f"{ds.T.long_name} [solid]", fontsize=24)
+ax1.plot(ds.time, ds.T, color="blue", linestyle="-", linewidth=2.0)
+ax1.tick_params(axis="both", labelsize=20)
 ax1.set_xlim(xmin=1970, xmax=1973)
 ax1.set_ylim(ymin=0.0, ymax=16.0)
-ax1.set_yticks(np.arange(0, 16, 3))
+ax1.set_yticks(np.arange(0, 17, 3))
 
 
 ax2 = ax1.twinx()
 nclize_axis(ax2)
 
 
-color = "red"
-linestyle = "--"
 # we already handled the x-label with ax1
-ax2.set_ylabel(f"{ds.P.long_name} [dash]", fontsize=fontsize)
-ax2.plot(ds.time, ds.P, color=color, linestyle=linestyle, linewidth=2.0)
-ax2.tick_params(axis="both", labelsize=labelsize)
-ax2.set_ylim(ymin=1008.0, ymax=1024.0)
-ax2.set_yticks(np.arange(1008, 1024, 3))
+
+ax2.set_ylabel(f"{ds.P.long_name} [dash]", fontsize=24)
+ax2.plot(ds.time, ds.P, color="red", linestyle="--", linewidth=2.0, dashes=[6.5, 3.7])
+ax2.tick_params(axis="both", labelsize=20)
+ax2.set_ylim(ymin=1008, ymax=1024.0)
+ax2.set_yticks(np.arange(1008, 1025, 3))
 
 
-plt.title("Curves Offset", fontsize=fontsize)
+plt.title("Curves Offset", fontsize=24, fontweight="bold", y=1.05)
 
 fig.tight_layout()
 plt.show()
