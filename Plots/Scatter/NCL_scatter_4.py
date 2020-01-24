817e58ad8cd78aa1048eaf275becf123a71bfdd7
Julia Kent <jkent@ucar.edu>
2020-01-24 11:58:15 -0700

    corrected min_periods option in scatter_4

--- Plots/Scatter/NCL_scatter_4.py
+++ Plots/Scatter/NCL_scatter_4.py
@@ -25,20 +25,20 @@ ts = ds.TS.sel(lat = 60, lon = 180, method = 'nearest')
 # Smooth data so that seasonal cycle is less 
 # prominent. This is for demo purposes only 
 # so that the regression line is more sloped.
-ts_rolled = ts.rolling(time=40, min_periods=1, center=True).mean()
+ts_rolled = ts.rolling(time=40, center=True).mean()
 
 ###############################################################################
 # 
 # calculate regression line
 m, b = np.polyfit(ts_rolled.time, ts_rolled.values, 1)
-regline_vals = [m * x + b for x in ts_rolled.time]
+regline_vals = [m * x + b for x in ts.time]
 
 ###############################################################################
 # 
 # create plot
 plt.figure(figsize=(6,6))
 plt.scatter(ts_rolled.time, ts_rolled.values, c='r', s=3)
-plt.plot(ts_rolled.time, regline_vals, 'k')
+plt.plot(ts.time, regline_vals, 'k')
 
 plt.xlim([6000, 9500])
 plt.ylim([268.0, 271.5])
