564bc6c03400e7aeae23a2790f1d00b15bccb1cd
Michael Levy <mike.levy.work@gmail.com>
2020-01-24 10:26:46 -0700

    Add Deepak's truncate_colormap function
    Also, drop to discrete number of color levels and draw lines between colors on
    color bar. (And shrink the colorbar so it's more legible).

--- Plots/Vectors/vector_4.py
+++ Plots/Vectors/vector_4.py
@@ -11,9 +11,11 @@ https://www.ncl.ucar.edu/Applications/Scripts/vector_4.ncl
 # Import necessary packages
 import xarray as xr
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import cartopy
 import cartopy.crs as ccrs
+import cmaps
 
 ###############################################################################
 # Read in data from netCDF file
@@ -78,6 +80,20 @@ def nclize_axis(ax):
     )
 
 ###############################################################################
+# The NCL example uses a truncated colormap. Here's a small function that does that
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    """
+    Utility function that truncates a colormap. Copied from  https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
+    """
+
+    new_cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        name="trunc({n},{a:.2f},{b:.2f})".format(n=cmap.name, a=minval, b=maxval),
+        colors=cmap(np.linspace(minval, maxval, n)),
+    )
+    return new_cmap
+
+###############################################################################
 # Make the plot
 
 # Set up figure and axes
@@ -97,24 +113,22 @@ plt.yticks(range(-90, 91, 30))
 # 2. There is no matplotlib equivalent to "CurlyVector"
 # Q = plt.quiver(lon[::5], lat[2::3], u.data[2::3, ::5], v.data[2::3, ::5], color=colors,
 #                scale=(5/0.05), zorder=1, pivot="middle", width=0.001, headwidth=15)
-Q = plt.quiver(lon[::5], lat[2::3], u.data[2::3, ::5], v.data[2::3, ::5], t.data[2::3, ::5], cmap='rainbow',
+plt.cm.register_cmap('BlAqGrYeOrReVi200', truncate_colormap(cmaps.BlAqGrYeOrReVi200, minval=0.03, maxval=0.95, n=16))
+cmap = plt.cm.get_cmap('BlAqGrYeOrReVi200', 16)
+Q = plt.quiver(lon[::5], lat[2::3], u.data[2::3, ::5], v.data[2::3, ::5], t.data[2::3, ::5], cmap=cmap,
                scale=(5/0.05), zorder=1, pivot="mid", width=0.001, headwidth=15)
+plt.clim(228, 292)
 
 # Draw legend for vector plot
 qk = ax.quiverkey(Q, 0.875, 0.8, 5, r'5 $m/s$', labelpos='N',
                   coordinates='figure', color='black', zorder=2)
 
-cax = plt.axes((0.05, 0.025, 0.9, 0.05))
-cbar  =fig.colorbar(Q, ax=ax, label='Temperature ($^{\circ}$K)', cax=cax,
-                    orientation='horizontal', ticks=range(232,289,8))
+cax = plt.axes((0.225, 0.125, 0.55, 0.025))
+cbar = fig.colorbar(Q, ax=ax, label='Temperature ($^{\circ}$K)', cax=cax,
+                    orientation='horizontal', ticks=range(232,289,8), drawedges=True)
+
 # Turn on continent shading
-feature = cartopy.feature.NaturalEarthFeature(name='coastline',
-                                              category='physical',
-                                              scale='50m',
-                                              edgecolor='lightgray',
-                                              facecolor='lightgray',
-                                              zorder=0)
-ax.add_feature(feature)
+ax.add_feature(cartopy.feature.LAND, edgecolor='lightgray', facecolor='lightgray', zorder=0)
 
 # Generate plot!
 plt.show()
