69679678c7ea386f97a41943587e5885e64357b5
Michael Levy <mike.levy.work@gmail.com>
2020-01-24 23:28:51 -0700

    Include station_2_1 plot
    Using a legend insted of a colorbar is a little kludgy, perhaps because of how
    I draw the plot in the first place.

--- Plots/Station/station_2.py
+++ Plots/Station/station_2.py
@@ -48,32 +48,79 @@ nbins = len(colors) # One bin for each color
 cmap = mpl.colors.ListedColormap(colors)
 
 ###################################################
-# Plot station_2_2:
+# Both plots rely on same base figure, so
+# make a function to create desired plot
+# with no legend or colorbar
 
-# Set up figure
-fig, ax = plt.subplots(figsize=(10,6))
-ax = plt.axes(projection=ccrs.PlateCarree())
-gcv.util.nclize_axis(ax, minor_per_major=5)
-ax.xaxis.set_minor_locator(tic.AutoMinorLocator(n=4))
-gcv.util.add_lat_lon_ticklabels(ax)
+# Set up figure without colorbar or legend
+def make_shared_plot(figsize):
+    fig, ax = plt.subplots(figsize=figsize)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    gcv.util.nclize_axis(ax, minor_per_major=5)
+    ax.xaxis.set_minor_locator(tic.AutoMinorLocator(n=4))
+    gcv.util.add_lat_lon_ticklabels(ax)
 
-# Set major and minor ticks
-plt.xlim([-125,-70])
-plt.ylim([25,50])
-plt.xticks(range(-120, -75, 20))
-plt.yticks(range(30, 51, 10))
+    # Set major and minor ticks
+    plt.xlim([-125,-70])
+    plt.ylim([25,50])
+    plt.xticks(range(-120, -75, 20))
+    plt.yticks(range(30, 51, 10))
 
-# Turn on continent shading
-ax.add_feature(cartopy.feature.LAND, edgecolor='lightgray', facecolor='lightgray', zorder=0)
-ax.add_feature(cartopy.feature.LAKES, edgecolor='white', facecolor='white', zorder=0)
+    # Turn on continent shading
+    ax.add_feature(cartopy.feature.LAND, edgecolor='lightgray', facecolor='lightgray', zorder=0)
+    ax.add_feature(cartopy.feature.LAKES, edgecolor='white', facecolor='white', zorder=0)
 
-scatter = plt.scatter(lon, lat, c=dummy_data, cmap=cmap, zorder=1)
-plt.suptitle('Dummy station data colored according to range of values', y=.85)
-# colorbar
-cax = plt.axes((0.225, 0.125, 0.55, 0.025))
+    scatter = plt.scatter(lon, lat, c=dummy_data, cmap=cmap, zorder=1)
+    return scatter, ax
 
+###################################################
+# Make station_2_1 plot
+
+scatter1, ax = make_shared_plot(figsize=(10,5.5))
+
+# add legend
+def legend_func(x):
+    """
+        Returns appropriate legend label
+    """
+    print(x.shape)
+    return()
+
+plt.suptitle('Dummy station data colored according to range of values', y=.9)
+
+# Given how we generated the plot, adding a legend is a little kludgy
+# Basically, we draw a second plot where no data is in frame
+# but the legend for that plot is drawn where we want it
+lax = plt.axes((0, 0, 1, 0.1), frameon=False)
+# Plotting window is [0,1] x [0,1]
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.axis('off')
+for n, color in enumerate(colors):
+    if n == 0:
+        label = f'x < {bin_bounds[0]:.0f}'
+    elif n == nbins-1:
+        label = f'x >= {bin_bounds[-1]:.0f}'
+    else:
+        label = f'{bin_bounds[n-1]:.0f} <= x < {bin_bounds[n]:.0f}'
+    # Plotting data at (-10, -10) which is not in the plotting window
+    scatter = plt.scatter(-10, -10, color=color, label=label)
+
+# The legend for this second plot is what we are actually interested in
+# We want large font, no frame around the legend, and 4 columns of labels
+lax.legend(loc='center', fontsize='large', frameon=False, ncol=4)
+
+plt.show()
+
+###################################################
+# Make station_2_2 plot
+
+scatter2 = make_shared_plot(figsize=(10,5.25))
+plt.suptitle('Dummy station data colored according to range of values', y=.9)
+
+# add colorbar
+cax = plt.axes((0.225, 0.075, 0.55, 0.025))
 norm = mpl.colors.BoundaryNorm([-1.2] + bin_bounds+ [35], len(colors))
-
 cb2 = mpl.colorbar.ColorbarBase(cax, cmap=cmap,
                                 norm=norm,
                                 boundaries=[-1.2] + bin_bounds + [35],
