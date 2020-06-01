9809e8dd87f3e5d7e3b6b6e8c9ab59e3f974efb6
michaelavs <misi1684@ucar.edu>
2020-06-01 16:28:52 -0600

    Fixing issue with latitude extent on map projection

--- Plots/MapProjections/NCL_proj_2_lg.py
+++ Plots/MapProjections/NCL_proj_2_lg.py
@@ -25,7 +25,6 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import geocat.datafiles as gdf
-from geocat.viz import cmaps as gvcmaps
 from geocat.viz import util as gvutil
 
 ###############################################################################
@@ -37,6 +36,7 @@ t = ds.TS.isel(time=0)
 
 ###############################################################################
 # Fix the artifact of not-shown-data around 0 and 360-degree longitudes
+
 wrap_t = gvutil.xr_add_cyclic_longitudes(t, "lon")
 
 ###############################################################################
@@ -46,7 +46,7 @@ wrap_t = gvutil.xr_add_cyclic_longitudes(t, "lon")
 fig = plt.figure(figsize=(10, 10))
 
 # Generate axes using Cartopy and draw coastlines
-ax = plt.axes(projection=ccrs.Mercator())
+ax = plt.axes(projection=ccrs.Mercator(min_latitude= -84.5))
 ax.coastlines(linewidths=0.5)
 
 # Set extent to include latitudes from -90 to 89 and longitudes from -180
@@ -57,8 +57,8 @@ ax.set_extent([180, -180, -90, 89], ccrs.PlateCarree())
 gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=1, color='k', alpha=0.5)
 
 # Manipulate latitude and longitude gridline numbers and spacing
-gl.ylocator = mticker.FixedLocator(np.arange(-90, 91, 20))
-gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 30))
+gl.ylocator = mticker.FixedLocator(np.arange(-84.5, 91, 20))
+gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
 
 # Contourf-plot data (for filled contours)
 wrap_t.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),
