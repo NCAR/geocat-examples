702d16f930cfd289d245da31cf8bc0efb814c4bb
Rose Craker <hcraker@ucar.edu>
2020-06-02 14:21:11 -0400

    Added NCL_xy_5.py file

---
+++ Plots/XY/NCL_xy_5.py
@@ -0,0 +1,19 @@
+"""
+NCL_xy_5.py
+===============
+This script illustrates the following concepts:
+   - Adding a separate curve to an XY plot using gsn_polyline
+   - Drawing a Y reference line in an XY plot
+   - Filling the areas of an XY curve above and below a reference line
+   - Changing the width and height of a plot
+   - Creating a new date array to use in a plot
+   - Using named colors to indicate a fill color
+   - Changing the title on the Y axis
+   - Creating a main title
+   - Changing the size/shape of an XY plot using viewport resources
+   - Setting the mininum/maximum value of the Y axis in an XY plot
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/xy_5.ncl
+    - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/xy_5_1_lg.png
+"""
