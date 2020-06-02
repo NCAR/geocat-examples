268f310d44ffe133383ad053316fcf3f27c372fb
clairefio <clairefiorino@icloud.com>
2020-06-02 09:33:38 -0700

    Added ticks to main graph, added subplots to graph legend

--- Plots/Polygons/NCL_polyg_18.py
+++ Plots/Polygons/NCL_polyg_18.py
@@ -21,9 +21,16 @@ import cartopy
 from geocat.viz import util as gvutil
 import matplotlib.patches as mpatches
 ###############################################################################
+# Define helper function to remove ticks/frames from axes
 
+def removeTicks(axis):
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
 
-ax = plt.axes(projection=ccrs.PlateCarree())
+###############################################################################
+
+grid = plt.GridSpec(2, 20, hspace=0.2, wspace=0.2)
+ax = plt.subplot(grid[:-1, 1:], projection=ccrs.PlateCarree())
 
 # Add continents
 continents = cartopy.feature.NaturalEarthFeature(
@@ -38,16 +45,52 @@ ax.add_feature(continents)
 # Set map extent
 ax.set_extent([-180, 180, -90, 90], crs=ccrs.PlateCarree())
 
+# Create arrays with location and design of each marker
 lon = [-160, -140, -120, -100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140]
 lat = [-70, -60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 80]
 marker = ['.', '+', '*', 'o', 'x', 's', '^', 'v', 'D', '>', '<', 'p', 'h', '8', 'X', 'd']
+
+# Draw markers on diagonal line across graph
 for x in range(len(lon)):
     ax.scatter(lon[x], lat[x], marker=marker[x], color='blue', s=100, zorder = 3)
 
+# Draw small red box in upper center
 ax.add_patch(mpatches.Rectangle(xy=[7, 47], width=9, height=7, facecolor='None', edgecolor = 'red', alpha = 1.0, transform=ccrs.PlateCarree(), zorder=5)) 
-ax.add_patch(mpatches.Rectangle(xy=[110, -45], width=50, height=35, facecolor='limegreen', alpha = 0.2, transform=ccrs.PlateCarree(), zorder=5)) 
 
+# Draw green window in bottom right
+ax.add_patch(mpatches.Rectangle(xy=[110, -45], width=50, height=35, facecolor='lime', alpha = 0.3, transform=ccrs.PlateCarree(), zorder=5)) 
+
+gvutil.set_axes_limits_and_ticks(ax, xlim=None, ylim=None, xticks=[-180, -150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150, 180], yticks=[-90, -60, -30, 0, 30, 60, 90], xticklabels=None, yticklabels=None)
 gvutil.add_lat_lon_ticklabels(ax, zero_direction_label=True, dateline_direction_label=True)
 gvutil.set_titles_and_labels(ax, maintitle="Big centered title", maintitlefontsize=25)
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=3, labelsize="small")
+
+# Create second plot for legend
+ax2 = plt.subplot(grid[-1, 1:], frameon=False)
+removeTicks(ax2)
+
+axin1 = ax2.inset_axes([0.0, 0.3, .33, .5], frameon=False)
+removeTicks(axin1)
+axin1.add_artist(plt.Circle((0.5, 0.5), 0.2, color='blue'))
+
+axin2 = ax2.inset_axes([0.0, 0.5, .33, .5], frameon=False)
+removeTicks(axin2)
+axin2.text(0,.7,'Marker (left justified text)', color='blue', fontsize=9)
+
+axin3 = ax2.inset_axes([0.33, 0.3, .33, .5], frameon=False)
+removeTicks(axin3)
+axin3.plot([1, 2], [3, 3], color='red')
+
+axin4 = ax2.inset_axes([0.33, 0.5, .33, .5], frameon=False)
+removeTicks(axin4)
+axin4.text(0,.7,'Polyline (centered text)', color='red', fontsize=9)
+
+axin5 = ax2.inset_axes([0.66, 0.3, .33, .5], frameon=False)
+removeTicks(axin5)
+axin5.add_patch(mpatches.Rectangle(xy=[.3, .3], width=.6, height=.4, facecolor='lime', alpha = 0.3))
+
+axin6 = ax2.inset_axes([0.66, 0.5, .33, .5], frameon=False)
+removeTicks(axin6)
+axin6.text(0,.7,'Polygon (right justified text)', color='lime', fontsize=9)
 
 plt.show()
