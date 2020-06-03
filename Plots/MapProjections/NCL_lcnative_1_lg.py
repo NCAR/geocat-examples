325b763bf9cc3750e1542d57b506b5b576c7ebc0
michaelavs <misi1684@ucar.edu>
2020-06-03 16:37:10 -0600

    Chaning code to show gridlines

--- Plots/MapProjections/NCL_lcnative_1_lg.py
+++ Plots/MapProjections/NCL_lcnative_1_lg.py
@@ -19,15 +19,19 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import geocat.datafiles as gdf
 from geocat.viz import cmaps as gvcmaps
-
+from geocat.viz import util as gvutil
+import matplotlib.ticker as ticker
 
 ###############################################################################
 # Read in data:
 
 # Open a netCDF data file using xarray default engine and load the data into xarrays
 ds = xr.open_dataset(gdf.get("netcdf_files/pre.8912.mon.nc"))
+
 # Extract a slice of the data
 t = ds.pre[0,:]
+#print(t)
+#gvutil.xr_add_cyclic_longitudes(t,'lon')
 
 ###############################################################################
 # Plot:
@@ -35,16 +39,42 @@ t = ds.pre[0,:]
 # Generate figure
 plt.figure(figsize=(10, 10))
 
+
 # Generate axes using Cartopy and draw coastlines
 projection = ccrs.LambertConformal(central_longitude=45, standard_parallels=(36,55))
-ax = plt.axes(projection=projection, frameon=False)
+ax = plt.axes(projection=projection, frameon=True)
+print(ax)
 ax.set_extent((30, 55, 20, 45), crs=ccrs.PlateCarree())
 ax.coastlines(linewidth=0.5)
+
+ax.gridlines()
+
+ax.plot(np.arange(30,55,5))
+
 
 # Plot data and create colorbar
 newcmp = gvcmaps.BlueYellowRed
 t.plot.contourf(ax=ax, cmap=newcmp, transform=ccrs.PlateCarree(), levels = 14, cbar_kwargs={"orientation":"horizontal",  "ticks":np.arange(0, 240, 20),  "label":'', "shrink":0.9})
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+#Use geocat.viz.util convenience function to set axes tick values
+#gvutil.set_axes_limits_and_ticks(ax, xticks=np.linspace(30, 55, 6), yticks=np.linspace(20, 45, 6))
+
+#Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
+#gvutil.add_lat_lon_ticklabels(ax)
+
+#Use geocat.viz.util convenience function to add minor and major tick lines
+#gvutil.add_major_minor_ticks(ax, labelsize=12)
+
+#Use geocat.viz.util convenience function to add titles to left and right of the plot axis.
+#gvutil.set_titles_and_labels(ax, lefttitle='Anomalies: Surface Temperature', righttitle='K')
+
 plt.title(t.long_name, loc='left', size=16)
 plt.title(t.units, loc='right', size=16)
+
+
+
+
+
 plt.show()
 
