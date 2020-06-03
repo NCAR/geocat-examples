4f28133d02c97d988f8bd9014fa7c8bb9a7b44ec
clairefio <clairefiorino@icloud.com>
2020-06-03 10:54:31 -0700

    Fixed spacing of legend

--- Plots/Polygons/NCL_polyg_18.py
+++ Plots/Polygons/NCL_polyg_18.py
@@ -68,33 +68,46 @@ gvutil.add_lat_lon_ticklabels(ax, zero_direction_label=True, dateline_direction_
 gvutil.set_titles_and_labels(ax, maintitle="Big centered title", maintitlefontsize=25)
 gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=3, labelsize="small")
 
+###############################################################################
+# Plot legend
+
 # Create second plot for legend
 ax2 = plt.subplot(grid[-1, 1:], frameon=False)
 removeTicks(ax2)
 
+# Create 6 inset axes within subplot for each field in legend
+# Inset_axes positional array argument takes four values: 
+# [starting (bottom left) x coordinate of window, starting y coordinate of window, width of field, height of field]
+
+# Add circle image
 axin1 = ax2.inset_axes([0.1, 0.8, .1, .1], frameon=False)
 removeTicks(axin1)
 axin1.add_patch(mpatches.Circle((0.1, 0.1), radius=.1,color='blue', zorder=10))
 axin1.axis('equal')
 
+# Add label for circle
 axin2 = ax2.inset_axes([0.0, 0.65, .20, .5], frameon=False)
 removeTicks(axin2)
 axin2.text(0,.7,'Marker (left justified text)', color='blue', fontsize=12, verticalalignment='center')
 
-axin3 = ax2.inset_axes([0.33, 0.6, .33, .5], frameon=False)
+# Add red line
+axin3 = ax2.inset_axes([0.30, 0.6, .33, .5], frameon=False)
 removeTicks(axin3)
 axin3.plot([0, 4], [3, 3], color='red')
 axin1.axis('scaled')
 
+# Add label for red line
 axin4 = ax2.inset_axes([0.33, 0.65, .33, .5], frameon=False)
 removeTicks(axin4)
 axin4.text(0,.7,'Polyline (centered text)', color='red', fontsize=12,  verticalalignment='center')
 
-axin5 = ax2.inset_axes([0.66, 0.6, .33, .5], frameon=False)
+# Add green polygon 
+axin5 = ax2.inset_axes([0.62, 0.6, .33, .5], frameon=False)
 removeTicks(axin5)
 axin5.add_patch(mpatches.Rectangle(xy=[.3, .3], width=.6, height=.3, facecolor='lime', alpha = 0.3))
 axin1.axis('scaled')
 
+# Add label for green polygon
 axin6 = ax2.inset_axes([0.66, 0.65, .33, .5], frameon=False)
 removeTicks(axin6)
 axin6.text(0,.7,'Polygon (right justified text)', color='lime', fontsize=12, verticalalignment='center')
