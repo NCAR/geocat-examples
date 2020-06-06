e929f1ab76e7f4943de37328c0fa4e700825eb42
clairefio <clairefiorino@icloud.com>
2020-06-05 21:32:47 -0700

    Created initial plot with map, streamlines, and colorbar

---
+++ Plots/Streamlines/NCL_stream_9.py
@@ -0,0 +1,72 @@
+"""
+NCL_stream_9.py
+===============
+This script illustrates the following concepts:
+   - Showing features of the new color display model
+   - Using opacity to emphasize or subdue overlain features
+   - Using stLevelPalette resource to assign a color palette
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/stream_9.ncl
+    - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/stream_9_1_lg.png
+
+"""
+
+################################################################################
+# Import packages:
+import numpy as np
+import xarray as xr
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import cartopy.feature as cfeature
+import matplotlib.cm as cm
+import geocat.datafiles as gdf
+from geocat.viz import util as gvutil
+import matplotlib.colors as colors
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+################################################################################
+# Make color map 
+
+colormap = colors.ListedColormap(['darkblue', 'mediumblue', 'blue', 'cornflowerblue', 'skyblue', 'aquamarine',
+        'lime', 'greenyellow', 'gold', 'orange', 'orangered', 'red', 'maroon'])
+
+colorbounds = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52]
+
+norm = colors.BoundaryNorm(colorbounds, colormap.N)
+################################################################################
+# Read in data:
+
+# Open a netCDF data file using xarray default engine and load the data into xarrays
+ds1 = xr.open_dataset(gdf.get('netcdf_files/U500storm.cdf'))
+ds2 = xr.open_dataset(gdf.get('netcdf_files/V500storm.cdf'))
+
+################################################################################
+fig = plt.figure(figsize=(10, 15))
+
+#proj = ccrs.LambertAzimuthalEqualArea(central_longitude=-100, central_latitude=40)
+ax = fig.add_subplot(111, projection=ccrs.LambertAzimuthalEqualArea(central_longitude=-100, central_latitude=40), frameon=False, xbound=0.0, ybound=0.0)
+
+# Set axis projection
+#ax = plt.axes(projection=proj)
+# Set extent to include roughly the United States
+ax.set_extent((-128, -58, 18, 65), crs=ccrs.PlateCarree())
+ax.add_feature(cfeature.OCEAN, color='lightblue')
+ax.add_feature(cfeature.LAKES, color='white', edgecolor='black')
+ax.add_feature(cfeature.LAND, color='tan')
+ax.coastlines()
+
+# Extract a 2D horizontal slice from the first time step of the 3D U and V variables at the bottom level
+U = ds1.u.isel(timestep=0)
+V = ds2.v.isel(timestep=0)
+ax.streamplot(U.lon, U.lat, U.data, V.data, transform=ccrs.PlateCarree(), arrowstyle='->', linewidth=1, density=2.2, color=norm(U))
+
+axin1 = inset_axes(ax,
+                    width="75%",  
+                    height="3%",  
+                    loc='lower center'
+                    )
+
+cb = fig.colorbar(cm.ScalarMappable(cmap=colormap, norm=norm), cax=axin1, boundaries=colorbounds,
+                  ticks=colorbounds, spacing='uniform', orientation='horizontal', label='inches')
+
+plt.show()
