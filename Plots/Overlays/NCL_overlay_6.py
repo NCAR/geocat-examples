ebdb73fbe2181cff36f40022ef03cc114e2ca8ea
Rose Craker <hcraker@ucar.edu>
2020-06-09 15:33:17 -0400

    Changed color and outlines for lakes and ocean

--- Plots/Overlays/NCL_overlay_6.py
+++ Plots/Overlays/NCL_overlay_6.py
@@ -56,19 +56,20 @@ proj = ccrs.LambertAzimuthalEqualArea(central_longitude=-100, central_latitude=4
 # Set axis projection
 ax = plt.axes(projection=proj)
 # Set extent to include roughly the United States
+transparent = (0, 0, 0, 0) # RGBA value for a transparent color
 ax.set_extent((-128, -58, 18, 65), crs=ccrs.PlateCarree())
-ax.add_feature(cfeature.OCEAN, color='lightblue')
-ax.add_feature(cfeature.LAND, color='gray')
-ax.add_feature(cfeature.LAKES, color='white')
-ax.add_feature(cfeature.COASTLINE)
+ax.add_feature(cfeature.OCEAN, color='lightskyblue', zorder=0)
+ax.add_feature(cfeature.LAND, color='silver', zorder=1)
+ax.add_feature(cfeature.LAKES, linewidth=0.5, edgecolor='black', facecolor=transparent, zorder=3)
+ax.add_feature(cfeature.COASTLINE, linewidth=0.5, zorder=3)
 
 # Import color mab for pressure level contour
 p_cmap = gvcmaps.StepSeq25
-pressure = p.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap=p_cmap, levels=np.arange(975, 1050, 5), add_colorbar=False, add_labels=False)
+pressure = p.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap=p_cmap, levels=np.arange(975, 1050, 5), add_colorbar=False, add_labels=False, zorder=2)
 cbar = plt.colorbar(pressure, ticks=np.arange(980, 1045, 5), label = 'Sea Level Pressure')
 
 # Add streamline overlay
-plt.streamplot(u500.lon, u500.lat, u500.data, v500.data, transform=ccrs.PlateCarree(), color='black', arrowstyle='->', linewidth=0.5, density=2)
+plt.streamplot(u500.lon, u500.lat, u500.data, v500.data, transform=ccrs.PlateCarree(), color='black', arrowstyle='->', linewidth=0.5, density=2, zorder=4)
 
 # Add vector grid
 # First thin the data the the vector grid is less cluttered
@@ -79,7 +80,6 @@ v = v.data[0:33:2,0:36:2]
 
 # Import color map for vectors
 #vect_cmap = gvcmaps.amwg_blueyellowred
-Q = plt.quiver(x, y, u, v, transform=ccrs.PlateCarree())
+Q = plt.quiver(x, y, u, v, transform=ccrs.PlateCarree(), zorder=5)
 plt.quiverkey(Q, 0.95, 0.05, 20, label="20")
 plt.show()
-
