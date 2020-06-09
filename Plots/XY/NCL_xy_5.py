7cea4bd63eb4cc686f9f71198d367afb6c953d72
Rose Craker <hcraker@ucar.edu>
2020-06-09 12:38:52 -0400

    Edited comments and formatting

--- Plots/XY/NCL_xy_5.py
+++ Plots/XY/NCL_xy_5.py
@@ -2,15 +2,12 @@
 NCL_xy_5.py
 ===============
 This script illustrates the following concepts:
-   - Adding a separate curve to an XY plot using gsn_polyline
+   - Draw multiple curves on an XY plot
    - Drawing a Y reference line in an XY plot
    - Filling the areas of an XY curve above and below a reference line
-   - Changing the width and height of a plot
-   - Creating a new date array to use in a plot
    - Using named colors to indicate a fill color
-   - Changing the title on the Y axis
+   - Creating array of dates to use as x-axis tick labels
    - Creating a main title
-   - Changing the size/shape of an XY plot using viewport resources
    - Setting the mininum/maximum value of the Y axis in an XY plot
 
 See following URLs to see the reproduced NCL plot & script:
@@ -36,10 +33,11 @@ dsoik = ds.DSOI_KET
 dsoid = ds.DSOI_DEC
 date = ds.date
 
-# Creating a new date array for x axis labels
+# Creating a new array for x axis labels
 datedim = np.shape(date)[0]
 new_date = np.empty_like(date)
-
+# Dates in the file are represented by year and month
+# Create array that represents data by year and months as a fraction of a year
 for n in np.arange(0, datedim, 1):
     yyyy = date[n]/100
     mon = date[n]-yyyy*100
@@ -56,10 +54,14 @@ ax = plt.gca()
 plt.plot([0, datedim], [0, 0], color='grey', linewidth=0.75)
 
 # Plot data
+# _labels=False prevents axis labels from being drawn
 dsoik.plot.line(ax=ax, color='black', linewidth=0.5, _labels=False)
+dsoid.plot.line(ax=ax, color='black', _labels=False)
+
+# Fill above and below the 0 line
 ax.fill_between(dsoik.time, dsoik, where=dsoik>0, color='red')
 ax.fill_between(dsoik.time, dsoik, where=dsoik<0, color='blue')
-dsoid.plot.line(ax=ax, color='black', _labels=False)
+
 
 # Use geocat.viz.util convenience function to add minor and major tick lines
 gvutil.add_major_minor_ticks(ax, x_minor_per_major=4, y_minor_per_major=5, 
