a4d565b0bb5869ee7e630e0c9f522939ac2cd33d
Rose Craker <hcraker@ucar.edu>
2020-06-11 09:38:48 -0400

    Fixed colorbar positions and formatting

--- Plots/Overlays/NCL_overlay_6.py
+++ Plots/Overlays/NCL_overlay_6.py
@@ -19,6 +19,7 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.colors as mcolors
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
@@ -52,11 +53,20 @@ t = (t - 273.15) * 9/5 + 32
 
 ###############################################################################
 # Create map:
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(8,7))
 proj = ccrs.LambertAzimuthalEqualArea(central_longitude=-100, central_latitude=40)
 
 # Set axis projection
-ax = plt.axes(projection=proj)
+ax = plt.axes([0, 0.2, 0.8, 0.7], projection=proj)
+cax1 = inset_axes(ax, width='5%', height='100%', loc='lower right',
+                   bbox_to_anchor=(0.1, 0, 1, 1),
+                   bbox_transform=ax.transAxes,
+                   borderpad=0)
+cax2 = inset_axes(ax, width='100%', height='7%', loc='lower left',
+                   bbox_to_anchor=(0, -0.1, 1, 1),
+                   bbox_transform=ax.transAxes,
+                   borderpad=0)
+
 # Set extent to include roughly the United States
 transparent = (0, 0, 0, 0) # RGBA value for a transparent color
 ax.set_extent((-128, -58, 18, 65), crs=ccrs.PlateCarree())
@@ -68,10 +78,10 @@ ax.add_feature(cfeature.COASTLINE, linewidth=0.5, zorder=3)
 # Import color mab for pressure level contour
 p_cmap = gvcmaps.StepSeq25
 pressure = p.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap=p_cmap, levels=np.arange(975, 1050, 5), add_colorbar=False, add_labels=False, zorder=2)
-cbar = plt.colorbar(pressure, ticks=np.arange(980, 1045, 5), label = 'Sea Level Pressure')
+plt.colorbar(pressure, cax=cax1, ticks=np.arange(980, 1045, 5), label = 'Sea Level Pressure')
 
 # Add streamline overlay
-plt.streamplot(u500.lon, u500.lat, u500.data, v500.data, transform=ccrs.PlateCarree(), color='black', arrowstyle='->', linewidth=0.5, density=2, zorder=4)
+ax.streamplot(u500.lon, u500.lat, u500.data, v500.data, transform=ccrs.PlateCarree(), color='black', arrowstyle='->', linewidth=0.5, density=2, zorder=4)
 
 # Add vector grid
 # First thin the data the the vector grid is less cluttered
@@ -86,8 +96,8 @@ wind_cmap = gvcmaps.amwg_blueyellowred
 bounds = np.arange(-30,120,10)
 norm = mcolors.BoundaryNorm(bounds, wind_cmap.N)
 
-Q = plt.quiver(x, y, u, v, t, transform=ccrs.PlateCarree(), zorder=5, headwidth=5, cmap=wind_cmap, norm=norm)
-plt.colorbar(Q, ax=ax, ticks=np.arange(-20,110,10), norm=norm, orientation='horizontal', label='Surface Temperature')
+Q = ax.quiver(x, y, u, v, t, transform=ccrs.PlateCarree(), zorder=5, headwidth=5, cmap=wind_cmap, norm=norm)
+plt.colorbar(Q, cax=cax2, ticks=np.arange(-20,110,10), norm=norm, orientation='horizontal', label='Surface Temperature')
 
 ax.quiverkey(Q, 0.925, 0.025, 20, label='20', zorder=5)
 ax.add_patch(mpatches.Rectangle(xy=[0.85, 0], width=0.15, height=0.0925, facecolor='white', transform=ax.transAxes, zorder=4))
