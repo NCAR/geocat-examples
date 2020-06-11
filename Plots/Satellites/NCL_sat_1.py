6b174fdeef0450b20721fdf983871c9226bead4c
clairefio <clairefiorino@icloud.com>
2020-06-11 12:26:06 -0700

    Added tight_layout

--- Plots/Satellites/NCL_sat_1.py
+++ Plots/Satellites/NCL_sat_1.py
@@ -31,21 +31,26 @@ import cartopy.feature as cfeature
 # Open a netCDF data file using xarray default engine and load the data into xarrays
 ds = xr.open_dataset(gdf.get("netcdf_files/slp.1963.nc"), decode_times=False)
 
+# Get lon/lat data from the 24th timestep
 U = ds.slp[24, :, :]
 
+# Translate short values to float values
 U = U.astype('float64')
 
+# Convert Pa to hPa data
 U = U*0.01
 
+# Fix the artifact of not-shown-data around 0 and 360-degree longitudes
 wrap_U = gvutil.xr_add_cyclic_longitudes(U, "lon")
 
 ###############################################################################
 # Create plot
 
+# Set figure size
 fig = plt.figure(figsize=(8, 8))
 
 proj=ccrs.Orthographic(central_longitude=270, central_latitude=45)
-ax = plt.axes(projection=proj, frameon=True)
+ax = plt.axes(projection=proj)
 ax.set_global()
 
 ax.add_feature(cfeature.LAND, facecolor='lightgray')
@@ -63,12 +68,14 @@ p = wrap_U.plot.contour(ax=ax,
 ax.clabel(p, inline=True, fontsize=10, colors='k', fmt="%.0f")
 
 # Use gvutil function to set title and subtitles
-gvutil.set_titles_and_labels(ax, maintitle=r"$\bf{SLP}$"+" "+r"$\bf{1963,}$"+" "+r"$\bf{January}$"+" "+r"$\bf{24th}$", maintitlefontsize=18, lefttitle="mean Daily Sea Level Pressure", lefttitlefontsize=12, righttitle="hPa", righttitlefontsize=12)
+gvutil.set_titles_and_labels(ax, maintitle=r"$\bf{SLP}$"+" "+r"$\bf{1963,}$"+" "+r"$\bf{January}$"+" "+r"$\bf{24th}$", maintitlefontsize=20, lefttitle="mean Daily Sea Level Pressure", lefttitlefontsize=16, righttitle="hPa", righttitlefontsize=16)
 
 # Characteristics of text box
 props = dict(facecolor='white', edgecolor='black', alpha=0.5)
 
 # Place text box
-ax.text(0.40, -0.1, 'CONTOUR FROM 948 TO 1064 BY 4', transform=ax.transAxes, fontsize=18, bbox=props, zorder=5)
+ax.text(0.40, -0.1, 'CONTOUR FROM 948 TO 1064 BY 4', transform=ax.transAxes, fontsize=16, bbox=props, zorder=5)
+
+plt.tight_layout()
 
 plt.show()
