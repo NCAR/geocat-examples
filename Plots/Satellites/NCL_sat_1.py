90c407e2c68fba88ba25ee11b4259fafb3d45f36
clairefio <clairefiorino@icloud.com>
2020-06-11 11:45:41 -0700

    Added colors to projection, titles, and text box. Fixed central longitude/latitude and contour levels to match NCL plot

--- Plots/Satellites/NCL_sat_1.py
+++ Plots/Satellites/NCL_sat_1.py
@@ -2,7 +2,7 @@
 NCL_sat_1.py
 ===============
 This script illustrates the following concepts:
-   - Using 'short2flt' to unpack 'short' data
+   - Using 'astype' to unpack 'short' data
    - Drawing line contours over a satellite map
    - Changing the view of a satellite map
 
@@ -31,27 +31,44 @@ import cartopy.feature as cfeature
 # Open a netCDF data file using xarray default engine and load the data into xarrays
 ds = xr.open_dataset(gdf.get("netcdf_files/slp.1963.nc"), decode_times=False)
 
-U = ds.slp[1, :, :]
+U = ds.slp[24, :, :]
+
+U = U.astype('float64')
+
+U = U*0.01
+
 wrap_U = gvutil.xr_add_cyclic_longitudes(U, "lon")
 
 ###############################################################################
 # Create plot
 
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(8, 8))
 
-proj=ccrs.Orthographic(central_longitude=-90, central_latitude=45)
-ax = plt.axes(projection=proj)
+proj=ccrs.Orthographic(central_longitude=270, central_latitude=45)
+ax = plt.axes(projection=proj, frameon=True)
 ax.set_global()
 
 ax.add_feature(cfeature.LAND, facecolor='lightgray')
-
+ax.add_feature(cfeature.COASTLINE, linewidth=.5)
+ax.add_feature(cfeature.OCEAN, facecolor='lightcyan')
 
 p = wrap_U.plot.contour(ax=ax,
                         transform=ccrs.PlateCarree(),
                         linewidths=0.5,
-                        levels=30,
+                        levels=15,
                         cmap='black',
                         add_labels=False)
 
+# Label contours
+ax.clabel(p, inline=True, fontsize=10, colors='k', fmt="%.0f")
+
+# Use gvutil function to set title and subtitles
+gvutil.set_titles_and_labels(ax, maintitle=r"$\bf{SLP}$"+" "+r"$\bf{1963,}$"+" "+r"$\bf{January}$"+" "+r"$\bf{24th}$", maintitlefontsize=18, lefttitle="mean Daily Sea Level Pressure", lefttitlefontsize=12, righttitle="hPa", righttitlefontsize=12)
+
+# Characteristics of text box
+props = dict(facecolor='white', edgecolor='black', alpha=0.5)
+
+# Place text box
+ax.text(0.40, -0.1, 'CONTOUR FROM 948 TO 1064 BY 4', transform=ax.transAxes, fontsize=18, bbox=props, zorder=5)
 
 plt.show()
