0631266bbd87764eeed96180e4c002ee9979f411
Rose Craker <hcraker@ucar.edu>
2020-06-15 11:53:14 -0400

    Cleaned up code and formatting

--- Plots/Overlays/NCL_overlay_1.py
+++ Plots/Overlays/NCL_overlay_1.py
@@ -17,7 +17,6 @@ See following URLs to see the reproduced NCL plot & script:
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
@@ -56,33 +55,42 @@ ax.add_feature(cfeature.LAKES, linewidth=0.5, edgecolor='black',
 ax.add_feature(cfeature.COASTLINE, linewidth=0.5)
 
 # Plot filled contour
-temp = t.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap, levels=t_lev, extend='neither', add_colorbar=False, add_labels=False)
-plt.colorbar(temp, ax=ax, ticks=np.arange(215, 270, 5), orientation='horizontal', pad=0.075)
+temp = t.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                       levels=t_lev, extend='neither', add_colorbar=False,
+                       add_labels=False)
+plt.colorbar(temp, ax=ax, ticks=np.arange(215, 270, 5),
+             orientation='horizontal', pad=0.075)
 
 # Plot line contour
-wind = u.plot.contour(ax=ax, transform=ccrs.PlateCarree(), vmin=-5, vmax=35, levels=u_lev, colors='black', linewidths=0.5, add_labels=False)
+wind = u.plot.contour(ax=ax, transform=ccrs.PlateCarree(), vmin=-5, vmax=35,
+                      levels=u_lev, colors='black', linewidths=0.5,
+                      add_labels=False)
 # Manually specify where contour labels will go using lat and lon coordiantes
 manual = [(-107, 52), (-79, 57), (-78, 47), (-103, 32), (-86, 23)]
-clabels = ax.clabel(wind, levels=u_lev, fmt='%d', inline=True, fontsize=10, manual=manual)
-print(clabels)
+clabels = ax.clabel(wind, levels=u_lev, fmt='%d', inline=True, fontsize=10,
+                    manual=manual)
 
 # Add lower text box
 ax.text(1, -0.3, "CONTOUR FROM -5 TO 35 BY 5",
-        fontname='Helvetica',
         horizontalalignment='right',
         transform=ax.transAxes,
-        bbox=dict(boxstyle='square, pad=0.25', facecolor='white', edgecolor='black'))
+        bbox=dict(boxstyle='square, pad=0.25', facecolor='white',
+                  edgecolor='black'))
 
-gvutil.set_titles_and_labels(ax, maintitle=r"$\bf{T/U @500hPa}$", lefttitle=t.long_name, righttitle=t.units)
+gvutil.set_titles_and_labels(ax, maintitle=r"$\bf{T/U @500hPa}$",
+                             lefttitle=t.long_name, righttitle=t.units)
 ax.text(0, 1.01, u.long_name, transform=ax.transAxes)
 ax.text(0.97, 1.01, u.units, transform=ax.transAxes)
 
-# Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
+# Use geocat.viz.util convenience function to make plots look like NCL plots by
+# using latitude, longitude tick labels
 gvutil.add_lat_lon_ticklabels(ax)
-ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol= ''))
+ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
 ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
-gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=5, labelsize=12)
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=5,
+                             labelsize=12)
 # Use geocat.viz.util convenience function to set axes tick values
-gvutil.set_axes_limits_and_ticks(ax, xticks=np.arange(-120, -30, 30), yticks=np.arange(20, 70, 10))
+gvutil.set_axes_limits_and_ticks(ax, xticks=np.arange(-120, -30, 30),
+                                 yticks=np.arange(20, 70, 10))
 
 plt.show()
