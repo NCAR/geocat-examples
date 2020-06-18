629fd56ed1ae28910a626c4975b7f79f66a205e9
clairefio <clairefiorino@icloud.com>
2020-06-18 11:05:38 -0700

    Changed spacing of plot and colorbar, added appropriate comments to allow Sphinx to generate documentation

--- Plots/Polygons/NCL_polyg_19.py
+++ Plots/Polygons/NCL_polyg_19.py
@@ -1,18 +1,41 @@
-import shapefile as shp
+"""
+NCL_polyg_19.py
+===============
+This script illustrates the following concepts:
+   - Adding lines and polygons to a map
+   - Adding a map to another map as an annotation
+   - Coloring shapefile outlines based on an array of values
+   - Drawing a custom labelbar on a map
+   - Using functions for cleaner code
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/polyg_19.ncl
+    - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/polyg_19_lg.png
+"""
+
+###############################################################################
+# Import packages:
 import matplotlib.pyplot as plt 
 import matplotlib.gridspec as gridspec
-import numpy as np
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
+import matplotlib.colors as colors
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import matplotlib.cm as cm
+import shapefile as shp
+import numpy as np
+
 import geocat.datafiles as gdf
 from geocat.viz import util as gvutil
-import xarray as xr
-import matplotlib.colors as colors
-from matplotlib.colors import Normalize
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-from geocat.viz import util as gvutil
-import matplotlib.cm as cm
 
+###############################################################################
+# Read in data:
+us = shp.Reader("tl_2017_us_state\\tl_2017_us_state.dbf")
+
+statepopulationfile = open(gdf.get("ascii_files/us_state_population.txt"), 'r')
+
+###############################################################################
+# Set colormap data and colormap bounds:
 colormap = colors.ListedColormap(['lightcoral', 'wheat', 'palegoldenrod', 'powderblue', 'mediumpurple',
             'indianred', 'peru', 'dodgerblue', 'slateblue', 'firebrick', 'sienna', 'olivedrab',
             'steelblue', 'navy'])
@@ -21,7 +44,10 @@ colorbounds = [0, 1, 2.5, 3, 4, 5, 6, 7, 8, 9, 10, 12, 25, 38, 40]
 
 norm = colors.BoundaryNorm(colorbounds, colormap.N)
 
-def getFillColor(statepopulationfile):
+###############################################################################
+# Define helper function to get the populations of each state
+
+def getStatePopulations(statepopulationfile):
 
     nameandpopdict = {}
     Lines = statepopulationfile.read().splitlines()
@@ -36,6 +62,9 @@ def getFillColor(statepopulationfile):
     
     return nameandpopdict
 
+###############################################################################
+# Define helper function to get the color of each state based on its population
+
 def findDivColor(colorbounds, pdata):
 
     for x in range(len(colorbounds)):
@@ -48,14 +77,22 @@ def findDivColor(colorbounds, pdata):
             # Index is 'x-1' because colorbounds is one item longer than colormap
             return colormap.colors[x-1]
 
+###############################################################################
+# Define helper function to remove ticks from axes
+
 def removeTicks(axis):
 
     axis.get_xaxis().set_visible(False)
     axis.get_yaxis().set_visible(False)
 
+###############################################################################
+# Define helper function to plot and color each state
+
 def plotRegion(shape, axis, xlim):
 
+    # Plot each shape within a region (ex. mainland Alaska and all of it's surrounding Alaskan islands)
     for i in range(len(shape.shape.parts)):
+
         i_start = shape.shape.parts[i]
         if i==len(shape.shape.parts)-1:
             i_end = len(shape.shape.points)
@@ -75,7 +112,7 @@ def plotRegion(shape, axis, xlim):
                 x.append(i[0])
                 y.append(i[1])
 
-        # FILL WITH COLOR
+        # Fill each state with color
         abbrevname = shape.record.STUSPS
         pop = nameandpopdict[abbrevname]
         color = findDivColor(colorbounds, pop)
@@ -85,33 +122,38 @@ def plotRegion(shape, axis, xlim):
 
         pc = PatchCollection(patches, match_original=True, edgecolor='k', linewidths=1., zorder=2)
         axis.add_collection(pc)
-    
+
+###############################################################################
+# Plot:
+
+# Create figure
 fig = plt.figure(figsize=(8, 8))
 spec = gridspec.GridSpec(ncols=1, nrows=2, hspace=0.05, wspace=0.1, figure=fig, height_ratios=[2, 1])
 
+# Create upper axis
 ax1 = fig.add_subplot(spec[0, 0], frameon=False)
 removeTicks(ax1)
 
+# Create lower axis
 ax2 = fig.add_subplot(spec[1, 0], frameon=False)
 removeTicks(ax2)
 
-axin1 = ax2.inset_axes([0.0, 0.4, 0.30, 0.80], frameon=False)
+# Create three inset axes on lower axis for Alaska, Hawaii, and Puerto Rico
+axin1 = ax2.inset_axes([0.0, 0.7, 0.30, 0.80], frameon=False)
 removeTicks(axin1)
-
-axin2 = ax2.inset_axes([0.40, 0.4, 0.20, 0.40], frameon=False)
+axin2 = ax2.inset_axes([0.40, 0.7, 0.20, 0.40], frameon=False)
 removeTicks(axin2)
-
-axin3 = ax2.inset_axes([0.70, 0.4, 0.30, 0.30], frameon=False)
+axin3 = ax2.inset_axes([0.70, 0.7, 0.30, 0.30], frameon=False)
 removeTicks(axin3)
 
-us = shp.Reader("tl_2017_us_state\\tl_2017_us_state.dbf")
-
+# Specify which shapes not to include when plotting the mainland U.S.
 nonmainlandus = ['Commonwealth of the Northern Mariana Islands', 'Guam', 'Puerto Rico', 'Hawaii', 'Alaska', 'American Samoa', 
                  'United States Virgin Islands']  
 
-statepopulationfile = open(gdf.get("ascii_files/us_state_population.txt"), 'r')
-nameandpopdict = getFillColor(statepopulationfile)
+# Get population of each state
+nameandpopdict = getStatePopulations(statepopulationfile)
 
+# For each shape in shapefile, plot on map in appropriate axis
 for shape in us.shapeRecords():
     
     if shape.record.NAME not in nonmainlandus:
@@ -126,18 +168,20 @@ for shape in us.shapeRecords():
     elif shape.record.NAME == 'Puerto Rico':
         plotRegion(shape, axin3, [None, None])
 
+# Set title using helper function from geocat-viz
 title = r"$\bf{Population}$"+" "+r"$\bf{in}$"+" "+r"$\bf{Millions}$"+" "+r"$\bf{(2014)}$"
 gvutil.set_titles_and_labels(ax1, maintitle=title,
                              maintitlefontsize=18)
 
-axins1 = inset_axes(ax2,
+# Create fourth inset axis for colorbar
+axin4 = inset_axes(ax2,
                     width="115%",
                     height="12%",
-                    loc='lower center'
+                    loc='center'
                     )
 
-# Add colorbar to plot
-cb = fig.colorbar(cm.ScalarMappable(cmap=colormap, norm=norm), cax=axins1, boundaries=colorbounds,
+# Create colorbar
+cb = fig.colorbar(cm.ScalarMappable(cmap=colormap, norm=norm), cax=axin4, boundaries=colorbounds,
                   ticks=colorbounds[1:-1], spacing='uniform', orientation='horizontal', label='inches')
 
 plt.show()
