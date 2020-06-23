8bffa262379f38966233a9446e1869f1b80ad741
Rose Craker <hcraker@ucar.edu>
2020-06-23 12:05:46 -0400

    Removed print statements and fixed formattin

--- Plots/XY/NCL_xy_4.py
+++ Plots/XY/NCL_xy_4.py
@@ -17,7 +17,8 @@ Ways of specifying marks:
     - matplotlib.markers has an extensive [list](https://matplotlib.org/3.2.1/api/markers_api.html) of predefined markers
     - Mathematical symbols described [here](https://matplotlib.org/3.2.1/tutorials/text/mathtext.html) can be used
     - Unicode characters
-    - If you still cannot find the symbol you are looking for, a custom made [Path](https://matplotlib.org/3.2.1/api/path_api.html#matplotlib.path.Path) instance can be used to draw your own marker
+    - If you still cannot find the symbol you are looking for, a custom made
+      [Path](https://matplotlib.org/3.2.1/api/path_api.html#matplotlib.path.Path) instance can be used to draw your own marker
 """
 
 ###############################################################################
@@ -51,21 +52,24 @@ ax = plt.axes()
 plt.scatter(t.time, t.data, color='red')
 
 # Use geocat.viz.util convenience function to set titles and labels
-gvutil.set_titles_and_labels(ax, maintitle="Scatter Plot", xlabel=t['time'].long_name, ylabel=t.long_name)
+gvutil.set_titles_and_labels(ax, maintitle="Scatter Plot",
+                             xlabel=t['time'].long_name, ylabel=t.long_name)
 
 # Use geocat.viz.util convenience function to add minor and major tick lines
-gvutil.add_major_minor_ticks(ax, x_minor_per_major=5, y_minor_per_major=4, labelsize=14)
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=5, y_minor_per_major=4,
+                             labelsize=14)
 
-# Calculate xlim by rounding the min value down and the max value up to a multiple of 5
+# Calculate xlim by rounding the min value down and the max value up to a
+# multiple of 5
 xmin = 5 * math.floor(t.time.min().data / 5)
 xmax = 5 * math.ceil(t.time.max().data / 5)
 
 gvutil.set_axes_limits_and_ticks(ax, xlim=(xmin, xmax),
                                  ylim=(220.0, 232.0),
-                                 xticklabels=[' ', 131160, ' ', 131170, ' ', 131180, ' ', 131190],
+                                 xticklabels=[' ', 131160, ' ', 131170, ' ',
+                                              131180, ' ', 131190],
                                  yticklabels=np.arange(220.0, 233.0, 2.0))
-print(t.data.min())
-print(t.data.max())
+
 plt.show()
 
 ###############################################################################
@@ -73,7 +77,8 @@ plt.show()
 plt.figure(figsize=(8, 8))
 ax = plt.axes()
 
-# Divide the data into arbitrary sections so each can be drawn with a different marker
+# Divide the data into arbitrary sections so each can be drawn with a different
+# type of marker
 data1 = t.data[0:8]
 time1 = t.time[0:8]
 
@@ -91,10 +96,12 @@ time4 = t.time[24:]
 plt.scatter(time1, data1, color='blue', marker='s', label='matplotlib.markers')
 
 # Use a mathematical symbol for a marker
-plt.scatter(time2, data2, color='green', marker='$\Omega$', s=100, label='mathematical symbols')
+plt.scatter(time2, data2, color='green', marker='$\Omega$', s=100,
+            label='mathematical symbol')
 
 # Unicode symbol marker
-plt.scatter(time3, data3, color='black', marker='$\u2608$', s=100, label='unicode symbol')
+plt.scatter(time3, data3, color='black', marker='$\u2608$', s=100,
+            label='unicode symbol')
 
 # Create custom path for marker
 verts = [(-0.5, -0.5), (-0.5, 0.5), (0, 0), (0.5, 0.5), (0.5, -0.5), (0, 0)]
@@ -105,14 +112,17 @@ plt.scatter(time4, data4, color='red', marker=path, s=100, label='custom path')
 plt.legend()
 
 # Use geocat.viz.util convenience function to set titles and labels
-gvutil.set_titles_and_labels(ax, maintitle="Make your own marker", xlabel=t['time'].long_name, ylabel=t.long_name)
+gvutil.set_titles_and_labels(ax, maintitle="Make your own marker",
+                             xlabel=t['time'].long_name, ylabel=t.long_name)
 
 # Use geocat.viz.util convenience function to add minor and major tick lines
-gvutil.add_major_minor_ticks(ax, x_minor_per_major=5, y_minor_per_major=4, labelsize=14)
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=5, y_minor_per_major=4,
+                             labelsize=14)
 
 gvutil.set_axes_limits_and_ticks(ax, xlim=(xmin, xmax),
                                  ylim=(220.0, 232.0),
-                                 xticklabels=[' ', 131160, ' ', 131170, ' ', 131180, ' ', 131190],
+                                 xticklabels=[' ', 131160, ' ', 131170, ' ',
+                                              131180, ' ', 131190],
                                  yticklabels=np.arange(220.0, 233.0, 2.0))
 
 plt.show()
