342d17373958bdf952dcf6a73a624d4454391523
Rose Craker <hcraker@ucar.edu>
2020-06-24 18:52:30 -0400

    Editted comments to be more accurate and clear

--- Plots/Bar/NCL_bar_11.py
+++ Plots/Bar/NCL_bar_11.py
@@ -3,11 +3,10 @@ NCL_bar_11.py
 ===============
 This script illustrates the following concepts:
    - Drawing filled bars using solid colors
-   - Changing the aspect ratio of a bar plot
    - Setting the minimum/maximum value of the X and Y axis in a bar plot
-   - Overlaying XY plots on each other
    - Paneling bar plots
-   - Drawing a custom labelbar
+   - Drawing a custom legend
+   - Generating random data using numpy
 
 See following URLs to see the reproduced NCL plot & script:
     - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/bar_11.ncl
@@ -55,7 +54,7 @@ for row in range(0, 2):
         # Add overall figure title
         fig.suptitle('Paneling bar plots, dummy data', size=18, y=0.94)
 
-        # Add data to first subplot
+        # Add data to subplot
         axs[row][col].bar(x-width*3/2, data[panel][0][:], width,
                           edgecolor='black', linewidth=0.25, color='red',
                           label='first')
