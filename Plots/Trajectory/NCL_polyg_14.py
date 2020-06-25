4ee6efd6be0c142a6808a16977c37d1de5376018
michaelavs <misi1684@ucar.edu>
2020-06-25 12:36:56 -0600

    Updating code

--- Plots/Trajectory/NCL_polyg_14.py
+++ Plots/Trajectory/NCL_polyg_14.py
@@ -2,36 +2,84 @@
 ###############################################################################
 # Import packages:
 import numpy as np
-import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
 import cartopy.feature as cfeature
+from geographiclib.geodesic import Geodesic
 
-import geocat.datafiles as gdf
 from geocat.viz import util as gvutil
 
 ###############################################################################
 # Read in data:
+###############################################
 
-# Open a netCDF data file using xarray default engine and load the data into xarrays
-plt.figure()
+def Plot(color, row, col, pos, title, style, pt):
 
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_extent([-125,-60,15,65], ccrs.PlateCarree())
-ax.add_feature(cfeature.LAND, color='lightgrey')
-#ax.add_feature(cfeature.LAKES, color='white')
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(row, col, pos, projection=ccrs.PlateCarree())
+    
+    ax.set_extent([-125,-60,15,65], ccrs.PlateCarree())
+    ax.add_feature(cfeature.LAND, color='lightgrey')
+    
+    plt.plot([-120, -64], [20, 60], style, color= color,  transform=ccrs.Geodetic())
+    
+    # start location
+    lon_fr = -120
+    lat_fr = 20
+    # end location
+    lon_to = -64
+    lat_to = 60
+    
+    # This gets geodesic between the two points
+    # WGS84 ellipsoid is used
+    gl = Geodesic.WGS84.InverseLine(lat_fr, lon_fr, lat_to, lon_to)
+    
+    num_points = 10  #for points on geodesic
+    print("distance latitude longitude azimuth")
+    
+    # Compute points on the geodesic, and plot them as red dots
+    # gl.s13 is the total length of the geodesic
+    # the points are equally spaced by true distances, but not on the map
+    # due to the projection distortion
+    for ea in np.linspace(0, gl.s13, num_points):
+        g = gl.Position(ea, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
+        #print("{:.0f} {:.5f} {:.5f} {:.5f}".format(g['s12'], g['lat2'], g['lon2'], g['azi2']))
+        lon2 = g['lon2']
+        lat2 = g['lat2']
+        ax.plot(lon2, lat2, pt, transform=ccrs.PlateCarree())
+    
+    
+   # Use geocat.viz.util convenience function to set axes parameters without calling several matplotlib functions
+    # Set axes limits, and tick values
+    gvutil.set_axes_limits_and_ticks(
+    ax,
+    xlim=(-125,-60),
+    ylim=(15,65),
+    xticks=np.linspace(-180, 180, 13),
+    yticks=np.linspace(0, 80, 5))
+    
+    # Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
+    gvutil.add_lat_lon_ticklabels(ax)
+    
+    # Use geocat.viz.util convenience function to add minor and major tick lines
+    gvutil.add_major_minor_ticks(ax, labelsize=12)
+    
+    # Use geocat.viz.util convenience function to set titles and labels without calling several matplotlib functions
+    gvutil.set_titles_and_labels(
+        ax,
+        maintitle=title,
+        maintitlefontsize=16,
+        righttitlefontsize=14,
+        xlabel="",
+        ylabel="")
 
-plt.plot([-120, -64], [20, 60],'-', color='blue',  transform=ccrs.Geodetic())
-
-x1=np.linspace(-120,-64, 5)
 
 
-#Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
-gvutil.add_lat_lon_ticklabels(ax)
 
-#Use geocat.viz.util convenience function to add minor and major tick lines
-gvutil.add_major_minor_ticks(ax, labelsize=12)
+# plot first color map
+Plot("blue", 2, 2, 2, "1st method: Two Points and Great Circle Path", '-', 'blue')
 
-#Use geocat.viz.util convenience function to add titles to left and right of the plot axis.
-gvutil.set_titles_and_labels(ax, lefttitle='Anomalies: Surface Temperature', righttitle='K')
+# plot second color map
+Plot("red", 2, 2, 2, "2nd method: Two Points and Great Circle Path", '-', 'ko')
+
+
