911594607aa868a36b886895955aba75c4c5b1c7
Rose Craker <hcraker@ucar.edu>
2020-07-01 14:07:23 -0400

    Formatted axes with ticks, titles, and projections

--- Plots/Panels/NCL_panel_13.py
+++ Plots/Panels/NCL_panel_13.py
@@ -32,6 +32,41 @@ ds = xr.open_dataset(gdf.get("netcdf_files/uv300.nc"))
 ds = ds.isel(time=1).drop_vars('time')
 U = ds.U
 V = ds.V
+lon = ds['lon']
+lat = ds['lat']
 
 # Calculate the magnitude of the winds
-speed = np.sqrt(U.data**2 + V.data**2)
+magnitude = np.sqrt(U.data**2 + V.data**2)
+
+###############################################################################
+# Plot:
+
+# Create sublots and specify their projections
+projection = ccrs.PlateCarree()
+fig, axs = plt.subplots(2, 1, figsize=(8, 8), subplot_kw={"projection": projection}, gridspec_kw={'hspace': 0.3})
+
+# Add coastlines
+axs[0].coastlines(linewidth=0.5)
+axs[1].coastlines(linewidth=0.5)
+
+# Use geocat.viz.util convenience function to set axes tick values
+gvutil.set_axes_limits_and_ticks(axs[0], xticks=np.arange(-180, 181, 30),
+                                 yticks=np.arange(-90, 91, 30))
+gvutil.set_axes_limits_and_ticks(axs[1], xticks=np.arange(-180, 181, 30),
+                                 yticks=np.arange(-90, 91, 30))
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(axs[0])
+gvutil.add_major_minor_ticks(axs[1])
+
+# Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
+gvutil.add_lat_lon_ticklabels(axs[0])
+gvutil.add_lat_lon_ticklabels(axs[1])
+
+# Use geocat.viz.util convenience function to add main title as well as titles to left and right of the plot axes.
+gvutil.set_titles_and_labels(axs[0], lefttitle='Speed', lefttitlefontsize=10,
+                             righttitle=U.units, righttitlefontsize=10)
+gvutil.set_titles_and_labels(axs[1], lefttitle='Wind', lefttitlefontsize=10,
+                             righttitle=U.units, righttitlefontsize=10)
+
+plt.show()
