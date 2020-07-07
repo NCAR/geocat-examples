f0bab98dbad777ee646a69acd6660ea6c7747450
Rose Craker <hcraker@ucar.edu>
2020-07-07 16:06:19 -0400

    Added loop to draw all shapes in the shapefile

--- Plots/Shapefiles/NCL_shapefiles_1.py
+++ Plots/Shapefiles/NCL_shapefiles_1.py
@@ -19,6 +19,7 @@ See following URLs to see the reproduced NCL plot & script:
 ###############################################################################
 # Import packages:
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import shapefile as shp
 import numpy as np
 import cartopy.crs as ccrs
@@ -31,13 +32,13 @@ from geocat.viz import util as gvutil
 # Read in data:
 
 # Open all shapefiles and associated .dbf, .shp, and .prj files so sphinx can run and generate documents
-file1 = open(gdf.get("shape_files/states.dbf"), 'r')
-file2 = open(gdf.get("shape_files/states.shp"), 'r')
-file3 = open(gdf.get("shape_files/states.shx"), 'r')
-file4 = open(gdf.get("shape_files/states.prj"), 'r')
+open(gdf.get("shape_files/states.dbf"), 'r')
+open(gdf.get("shape_files/states.shp"), 'r')
+open(gdf.get("shape_files/states.shx"), 'r')
+open(gdf.get("shape_files/states.prj"), 'r')
 
 # Open shapefiles
-shape = shp.Reader(gdf.get("shape_files/states.dbf"))
+shapefile = shp.Reader(gdf.get("shape_files/states.dbf"))
 
 ###############################################################################
 # Plot:
@@ -45,7 +46,23 @@ plt.figure(figsize=(10,8))
 ax = plt.axes(projection=ccrs.LambertConformal(standard_parallels=(33, 45), central_longitude=-98))
 ax.set_extent([-125, -64, 22, 50])
 
-ax.add_feature(cfeature.LAND, color='silver')
-ax.add_feature(cfeature.LAKES, color='white')
+ax.add_feature(cfeature.LAND, color='silver', zorder=0)
+ax.add_feature(cfeature.LAKES, color='white', zorder=1)
 
+for i in range(0, len(shapefile.shapes())):
+    shape = shapefile.shape(i)
+    record = shapefile.record(i)
+    if len(shape.parts) > 1: # if a shape has multiple parts make each one a separate patch
+        for j in range(0, len(shape.parts)):
+            start_index = shape.parts[j]
+            if (j is (len(shape.parts)-1)): # the last part uses the remaining points and doesn't require and end_index  
+                patch = mpatches.Polygon(shape.points[start_index:], edgecolor='black', transform=ccrs.PlateCarree(), zorder=2)
+            else:
+                end_index = shape.parts[j+1]
+                patch = mpatches.Polygon(shape.points[start_index : end_index], edgecolor='black', transform=ccrs.PlateCarree(), zorder=2)
+            ax.add_patch(patch)
+    else:
+        patch = mpatches.Polygon(shape.points, edgecolor='black', transform=ccrs.PlateCarree(), zorder=2)
+        ax.add_patch(patch)
+    
 plt.show()
