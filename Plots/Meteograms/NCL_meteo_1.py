45eed64995e7517b33808eb93297009cadd60ee6
clairefio <clairefiorino@icloud.com>
2020-07-13 09:11:41 -0700

    Changed layout by making axes 2/3 into inset axes, make minor aesthetic changes

--- Plots/Meteograms/NCL_meteo_1.py
+++ Plots/Meteograms/NCL_meteo_1.py
@@ -62,7 +62,6 @@ fig = plt.figure(figsize=(6,10))
 
 ax1 = fig.add_subplot(311, projection=ccrs.PlateCarree())
 ax2 = fig.add_subplot(312)
-ax3 = fig.add_subplot(313) 
 
 # Generate axes using Cartopy and draw coastlines
 ax1.coastlines(linewidths=0.5, alpha=-1)
@@ -72,7 +71,7 @@ ax1.set_aspect(1.5)
 cmap = gvcmaps.wgne15
 colors = ListedColormap(np.array(['white', 'honeydew', 'palegreen', 'limegreen', 'green', 'darkgreen']))
 
-bounds = [0, 20, 30, 50, 70, 85]
+bounds = [-20, -10, 0, 10, 20, 30, 50, 60, 70, 80, 90]
 
 norm = BoundaryNorm(boundaries=bounds, ncolors=5)
 
@@ -86,7 +85,7 @@ contour2 = ax1.contour(smoothrh, transform=ccrs.PlateCarree(), colors='black',
 
 # Plot filled contour
 contour3 = ax1.contour(smoothtemp, transform=ccrs.PlateCarree(), colors='red',
-                          levels=[0, 5, 10, 20, 30, 40, 50, 60], linewidths=0.4, linestyles='solid', zorder=3)
+                          levels=[-20, -10, 0, 10, 20, 30, 40, 50, 60], linewidths=0.4, linestyles='solid', zorder=3)
 
 #ax.clabel(contour2, manual=True)
 
@@ -96,8 +95,7 @@ contour3 = ax1.contour(smoothtemp, transform=ccrs.PlateCarree(), colors='red',
 #barbs = ax.barbs(taus, taus, ugrid, vgrid)
 
 
-gvutil.set_titles_and_labels(ax1, maintitle='Meteogram for LGSA, 28/12Z', maintitlefontsize=18, lefttitle=None, lefttitlefontsize=18, righttitle=None, righttitlefontsize=18,
-                          xlabel=None, ylabel='Pressure (mb)', labelfontsize=16)
+gvutil.set_titles_and_labels(ax1, maintitle='Meteogram for LGSA, 28/12Z', maintitlefontsize=18, ylabel='Pressure (mb)', labelfontsize=12)
 
 yticklabels = [1000, 975, 950, 925, 850, 700, 500, 400]
 
@@ -106,39 +104,57 @@ xticklabels = ['12z', '15z', '18z', '21z', 'Apr29', '03z', '06z', '09z', '12z', 
 
 gvutil.set_axes_limits_and_ticks(ax1, xlim=[0,24], ylim=[0,7], xticks=np.arange(0,25), yticks=np.arange(0,8), xticklabels=xticklabels, yticklabels=yticklabels)
 
+ax1.tick_params(axis="x", direction="in")
+ax1.tick_params(axis="y", labelsize=9) 
+ax1.yaxis.labelpad = -3
 for tick in ax1.get_xticklabels():
     tick.set_rotation(90)
 
-# Plot bar chart beneath it
+# Create text box
+ax1.text(1.0, -0.35, "CONTOUR FROM -20 TO 60 BY 10",
+        horizontalalignment='right',
+        transform=ax1.transAxes,
+        bbox=dict(boxstyle='square, pad=0.15', facecolor='white', edgecolor='black'))
 
-ax2.set_aspect(30)
+# Create inset axes below first axes
+axin1 = ax2.inset_axes([0.0, 0.3, 1, 0.30])
+axin2 = ax2.inset_axes([0.0, -.1, 1, 0.35])
+ax2.axis("off")
 
-ax2.bar(taus, rain03, width=3, color='limegreen', edgecolor='k', linewidth=.2)
+# Plot bar chart
 
-gvutil.set_titles_and_labels(ax2, ylabel='3hr rain total', labelfontsize=16)
+axin1.set_aspect(30)
+
+axin1.bar(taus, rain03, width=3, color='limegreen', edgecolor='k', linewidth=.2)
+
+gvutil.set_titles_and_labels(axin1, ylabel='3hr rain total', labelfontsize=12)
 
 yticklabels = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 
 xticklabels = ['12z', '', '18z', '', 'Apr29', '', '06z', '', '12z', '', '18z',
                '', 'Apr30', '', '06z', '', '12z', '', '18z', '', 'May01', '', '06z', '', '12z']
 
-gvutil.set_axes_limits_and_ticks(ax2, xlim=[0,72], ylim=[0,.5], xticks=np.arange(0, 75, 3), yticks=np.arange(0,.6,0.1), xticklabels=xticklabels, yticklabels=yticklabels) 
+gvutil.set_axes_limits_and_ticks(axin1, xlim=[0,72], ylim=[0,.5], xticks=np.arange(0, 75, 3), yticks=np.arange(0,.6,0.1), xticklabels=xticklabels, yticklabels=yticklabels) 
 
-gvutil.add_major_minor_ticks(ax2, y_minor_per_major=5, labelsize="small")
+gvutil.add_major_minor_ticks(axin1, y_minor_per_major=5, labelsize="small")
+axin1.yaxis.set_ticks_position('left')
+axin1.xaxis.set_ticks_position('bottom')
 
-# Plot bar chart beneath it
+# Plot line chart beneath it
 
-ax3.set_aspect(2)
+axin2.set_aspect(2)
 
-ax3.plot(taus, tempht, color='red')
+axin2.plot(taus, tempht, color='red')
 
-gvutil.set_titles_and_labels(ax3, ylabel='Temp at 2m', labelfontsize=16)
+gvutil.set_titles_and_labels(axin2, ylabel='Temp at 2m', labelfontsize=12)
 
 yticklabels = [59, 60, 61, 62, 63, 64]
 
-gvutil.set_axes_limits_and_ticks(ax3, xlim=[0,72], ylim=[59,64.5], xticks=np.arange(0, 75, 3), yticks=np.arange(59,65), xticklabels=xticklabels, yticklabels=yticklabels) 
+gvutil.set_axes_limits_and_ticks(axin2, xlim=[0,72], ylim=[59,64.5], xticks=np.arange(0, 75, 3), yticks=np.arange(59,65), xticklabels=xticklabels, yticklabels=yticklabels) 
 
-gvutil.add_major_minor_ticks(ax3, y_minor_per_major=5, labelsize="small")
+gvutil.add_major_minor_ticks(axin2, y_minor_per_major=5, labelsize="small")
+axin2.yaxis.set_ticks_position('left')
+axin2.xaxis.set_ticks_position('bottom')
 
 plt.subplots_adjust(hspace=-0.3)
 
