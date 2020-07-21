e2bf62bf222a4e898c255e3d7ff27dfd487a8830
Rose Craker <hcraker@ucar.edu>
2020-07-21 11:44:57 -0400

    Created NCL_polyg_8.py

---
+++ Plots/Polygons/NCL_polyg_8.py
@@ -0,0 +1,16 @@
+"""
+NCL_polyg_8.py
+==============
+This script illustrates the following concepts:
+   - Drawing a scatter plot on a map
+   - Changing the marker color and size in a map plot
+   - Plotting station locations using markers
+   - Manually creating a legend using markers and text
+   - Adding text to a plot
+   - Generating dummy data using "random_uniform"
+   - Binning data
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/polyg_8.ncl
+    - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/polyg_8_lg.png
+"""
