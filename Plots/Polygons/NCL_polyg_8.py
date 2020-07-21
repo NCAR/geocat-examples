e2228807635658df42fe506f3696f207be07f0b9
Rose Craker <hcraker@ucar.edu>
2020-07-21 17:23:01 -0400

    Plotted markers with different colors

--- Plots/Polygons/NCL_polyg_8.py
+++ Plots/Polygons/NCL_polyg_8.py
@@ -31,13 +31,13 @@ npts = 100
 random = np.random.default_rng(seed=1)
 # Create random coordinates to position the markers
 lat = random.uniform(low=25, high=50, size=npts)
-lon = random.uniform(low=235, high=290, size=npts)
+lon = random.uniform(low=-125, high=-70, size=npts)
 # Create random data which the color will be based off of
 r = random.uniform(low=-1.2, high=35, size=npts)
 
 bins = [0, 5, 10, 15, 20, 23, 26]
-colors = ['mediumpurple', 'blue', 'cyan', 'green', 'limegreen', 'greenyellow',
-          'yellow', 'orange']
+colors = ['mediumpurple', 'darkblue', 'blue', 'darkgreen', 'green', 'limegreen',
+          'yellow', 'darkorange']
 
 ###############################################################################
 # Plot:
@@ -47,8 +47,8 @@ ax = plt.axes(projection=projection)
 ax.set_extent([-125, -70, 25, 50], crs=projection)
 
 # Draw land
-ax.add_feature(cfeature.LAND, color='silver')
-ax.add_feature(cfeature.LAKES, color='white')
+ax.add_feature(cfeature.LAND, color='silver', zorder=0)
+ax.add_feature(cfeature.LAKES, color='white', zorder=0)
 
 # Use geocat.viz.util convenience function to set axes tick values
 gvutil.set_axes_limits_and_ticks(ax, xticks=np.linspace(-120, -80, 3),
@@ -67,4 +67,29 @@ gvutil.add_major_minor_ticks(ax, x_minor_per_major=4, y_minor_per_major=5 ,label
 gvutil.set_titles_and_labels(ax, maintitlefontsize=16,
                              maintitle="Dummy station data colored and\nsized according to range of values")
 
+# Plot markers with values less than first bin value
+masked_lon = np.where(r<bins[0], lon, np.nan)
+masked_lat = np.where(r<bins[0], lat, np.nan)
+label = "x < " + str(bins[0])
+plt.scatter(masked_lon, masked_lat, label=label, color=colors[0], zorder=1)
+
+# Plot all other markers but those in the last bin
+label_format = "{} <= x < {}"
+for x in range(1, len(bins)):
+    masked_lon = np.where(bins[x - 1] <= r, lon, np.nan)
+    masked_lon = np.where(r < bins[x], masked_lon, np.nan)
+    masked_lat = np.where(bins[x - 1] <= r, lat, np.nan)
+    masked_lat = np.where(r < bins[x], masked_lat, np.nan)
+    label = label_format.format(bins[x - 1], bins[x])
+    plt.scatter(masked_lon, masked_lat, label=label, color=colors[x], zorder=1)
+
+# Plot markers with values greater than or equal to last bin value
+masked_lon = np.where(r>=bins[-1], lon, np.nan)
+masked_lat = np.where(r>=bins[-1], lat, np.nan)
+label = "x >= " + str(bins[-1])
+plt.scatter(masked_lon, masked_lat, label=label, color=colors[-1], zorder=1)
+
+# `ncol` being equal to the half number of labels makes it appear horizontal with two rows
+legend = ax.legend(bbox_to_anchor=(-0.05, -0.3), ncol=4,
+                   loc='lower left', columnspacing=4.75, frameon=False)
 plt.show()
