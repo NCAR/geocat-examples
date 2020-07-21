e0cda1ea855be5f57ee68d2cd3661cd797501f27
michaelavs <michaela.sizemore@gmail.com>
2020-07-21 08:28:40 -0600

    Moving file to correct directory

--- Plots/MapProjections/NCL_native_2.py
+++ Plots/MapProjections/NCL_native_2.py
@@ -27,12 +27,13 @@ import matplotlib.ticker as mticker
 import cartopy.feature as cfeature
 
 import geocat.datafiles as gdf
+
 ###############################################################################
 # Read in data:
 # Open a netCDF data file using xarray default engine and
 # load the data into xarrays
 
-ds = xr.open_dataset(("1994_256_FSD.nc"), decode_times=False)
+ds = xr.open_dataset(gdf.get("netcdf_files/1994_256_FSD.nc"), decode_times=False)
 t = ds.FSD.isel(time=0)
 
 ###############################################################################
@@ -44,25 +45,39 @@ fig = plt.figure(figsize=(10, 10))
 # Generate axes using Cartopy and draw coastlines
 ax = plt.axes(projection=ccrs.Mercator())
 ax.coastlines(linewidths=0.5)
-ax.add_feature(cfeature.LAND, facecolor='lightgray')
+ax.add_feature(cfeature.LAND, facecolor="lightgray")
 
 # Set extent to include latitudes from 34 to 52 and longitudes from 128
 # to 144
 ax.set_extent([128, 144, 34, 52], ccrs.PlateCarree())
 
 # Contourf-plot data (for filled contours)
-pt = t.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), vmin=0, vmax=70,
-                     levels=15, cmap='inferno',
-                     cbar_kwargs={
+pt = t.plot.contourf(
+    ax=ax,
+    transform=ccrs.PlateCarree(),
+    vmin=0,
+    vmax=70,
+    levels=15,
+    cmap="inferno",
+    cbar_kwargs={
         "extendrect": True,
         "orientation": "vertical",
-        "ticks": np.arange(0,71,5),
-        "label": '',
-        })
+        "ticks": np.arange(0, 71, 5),
+        "label": "",
+    },
+)
 
 # Draw gridlines
-gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, dms=False, x_inline=False, y_inline=False, 
-                  linewidth=1, color='k', alpha=0.5)
+gl = ax.gridlines(
+    crs=ccrs.PlateCarree(),
+    draw_labels=True,
+    dms=False,
+    x_inline=False,
+    y_inline=False,
+    linewidth=1,
+    color="k",
+    alpha=0.5,
+)
 
 # Manipulate latitude and longitude gridline numbers and spacing
 gl.ylocator = mticker.FixedLocator(np.arange(128, 145, 20))
@@ -71,20 +86,22 @@ gl.top_labels = False
 gl.right_labels = False
 gl.xlines = False
 gl.ylines = False
-gl.xlocator = mticker.FixedLocator([130,134,138,142])
-gl.ylocator = mticker.FixedLocator([36,38,40,42,44,46,48,50])
-gl.xlabel_style = {'rotation': 0, 'size': 15}
-gl.ylabel_style = {'rotation': 0, 'size': 15}
+gl.xlocator = mticker.FixedLocator([130, 134, 138, 142])
+gl.ylocator = mticker.FixedLocator([36, 38, 40, 42, 44, 46, 48, 50])
+gl.xlabel_style = {"rotation": 0, "size": 15}
+gl.ylabel_style = {"rotation": 0, "size": 15}
 
-'''
+"""
 Currently, Cartopy does not have the ability to add tick marks on projections. 
 The GeoCAT Team is working to update the GeoCAT-viz convenience functions to 
 account for this 
-'''
+"""
 
-plt.title("Native Mercator Projection", loc='center', y=1.05, size=15, fontweight='bold')
-plt.title(t.units, loc='right', y=1.0,  size=14)
-plt.title("free surface deviation", loc='left', y=1.0, size=14)
+plt.title(
+    "Native Mercator Projection", loc="center", y=1.05, size=15, fontweight="bold"
+)
+plt.title(t.units, loc="right", y=1.0, size=14)
+plt.title("free surface deviation", loc="left", y=1.0, size=14)
 
 # Show the plot
 plt.show()
