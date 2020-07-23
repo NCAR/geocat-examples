0d96ef30047fd2bc955ea740837949f718dd63b6
Rose Craker <hcraker@ucar.edu>
2020-07-23 17:16:32 -0400

    Created first plot with highlighted line

--- Plots/XY/NCL_xy_12.py
+++ Plots/XY/NCL_xy_12.py
@@ -27,4 +27,37 @@ from geocat.viz import util as gvutil
 ds = xr.open_dataset(gdf.get("netcdf_files/uv300.nc"))
 
 # Extract slice of data
-U = ds.isel(time=0, drop=True).U
+U = ds.isel(time=0, lon=0, drop=True).U
+
+###############################################################################
+# Method 1: Splitting the line into parts and coloring them differently
+plt.figure(figsize=(8, 8))
+ax = plt.axes()
+
+bins = [0, 5, 20]
+# Slicing data is exclusive for the last value, to work around this we increment it
+start = U.data[bins[0]:bins[1]+1]
+highlight = U.data[bins[1]:bins[2]+1]
+end = U.data[bins[2]:]
+
+ax.plot(U.lat[bins[0]: bins[1]+1], start, color='black', linewidth=0.5)
+ax.plot(U.lat[bins[1]:bins[2]+1], highlight, color='red', linewidth=1)
+ax.plot(U.lat[bins[2]:], end, color='black', linewidth=0.5)
+
+# Use geocat.viz.util convenience function to set axes parameters
+gvutil.set_axes_limits_and_ticks(ax, ylim=(-10, 40), xlim=(-90, 90),
+                                 xticks=np.arange(-90, 91, 30),
+                                 yticks=np.arange(-10, 41, 10),
+                                 xticklabels=['90S', '60S', '30S', '0', '30N',
+                                              '60N', '90N'])
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, y_minor_per_major=5,
+                             labelsize=14)
+
+# Use geocat.viz.util convenience function to set titles and labels
+gvutil.set_titles_and_labels(ax, maintitle="Highlight Part of a Line",
+                             ylabel=U.long_name + " " + U.units)
+
+plt.show()
+
