015219cf6939ff8c3af6a3e6afd471e37fa4cc45
Rose Craker <hcraker@ucar.edu>
2020-07-29 14:15:21 -0400

    Created subplots using gridspec

--- Plots/Panels/NCL_dev_1.py
+++ Plots/Panels/NCL_dev_1.py
@@ -17,7 +17,7 @@ See following URLs to see the reproduced NCL plot & script:
     - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/dev_1_lg.png
 """
 
-###############################################################################
+##############################################################################
 # Import packages:
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
@@ -30,4 +30,26 @@ import geocat.datafiles as gdf
 from geocat.viz import cmaps as gvcmaps
 import geocat.viz.util as gvutil
 
-###############################################################################
+##############################################################################
+# Plot:
+
+# Specify projection for maps
+proj = ccrs.PlateCarree()
+
+# Generate figure (set its size (width, height) in inches)
+fig = plt.figure(figsize=(6, 8))
+grid = fig.add_gridspec(ncols=2, nrows=2, width_ratios=[0.85, 0.15])
+
+# Create axis for original data plot
+ax1 = fig.add_subplot(grid[0, 0], projection=ccrs.PlateCarree())
+ax1.coastlines(linewidths=0.5)
+
+# Create axis for zonal average plot
+ax2 = fig.add_subplot(grid[0, 1])
+
+# Create axis for deviation data plot
+ax3 = fig.add_subplot(grid[1, 0], projection=ccrs.PlateCarree())
+ax3.coastlines(linewidths=0.5)
+
+
+plt.show()
