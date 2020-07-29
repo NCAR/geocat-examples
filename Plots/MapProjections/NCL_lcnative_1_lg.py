25929c1e0f98e400dda03793d7db41c77aebe231
michaelavs <michaela.sizemore@gmail.com>
2020-07-29 09:08:14 -0600

    Updating code for formatting, clarity, and redundant code

--- Plots/MapProjections/NCL_lcnative_1_lg.py
+++ Plots/MapProjections/NCL_lcnative_1_lg.py
@@ -7,13 +7,19 @@ This script illustrates the following concepts:
     - Zooming in on a particular area on a Lambert Conformal map
     - Subsetting a color map
     - Using all three Cartopy Lambert projections to find the best fit for visualization
-    - Implementing best practices for choosing contour color scheme
+    - Implementing best practices for choosing contour color scheme. The original NCL 
+      version uses a rainbow color map which can create problems for individuals
+      impacted by color blindness and is not black and white printer friendly. 
+      To overcome this, we used the "Blues_r" color scheme from Matplotlib.
+      More information on best practices can be found `here <https://geocat-examples.readthedocs.io/en/latest/gallery/Colors/CB_Rain.html#sphx-glr-gallery-colors-cb-rain-py>`_.
+     
     
 See following URLs to see the reproduced NCL plot & script:
     - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/lcnative_1.ncl
     - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/lcnative_1_lg.png
 
 """
+
 ###############################################################################
 # Import packages:
 
@@ -23,7 +29,7 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
-
+from geocat.viz import util as gvutil
 from geocat.viz import cmaps as gvcmaps
 import geocat.datafiles as gdf
 
@@ -41,7 +47,6 @@ t = ds.pre.isel(time=0)
 
 plt.figure(figsize=(14, 14))
 
-
 def Plot(row, col, pos, proj, title):
     '''
     Args:
@@ -58,14 +63,14 @@ def Plot(row, col, pos, proj, title):
             center title of respective visualization
     '''    
     # Generate axes using Cartopy and draw coastlines
-    projection = proj
-    ax = plt.subplot(row, col, pos, projection=projection)
+    ax = plt.subplot(row, col, pos, projection=proj)
     ax.set_extent((28, 57, 20, 47), crs=ccrs.PlateCarree())
     ax.coastlines(linewidth=0.5)
-    # Import an NCL colormap
-    newcmp = gvcmaps.BlueYellowRed
-
     
+    # Import an NCL colormap
+    newcmp = "Blues_r"
+    
+    # Use ax "gridlines" function to draw lat/lon markers on projections
     gl = ax.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False)
     gl.top_labels = True
     gl.right_labels = True
@@ -78,7 +83,7 @@ def Plot(row, col, pos, proj, title):
     
     '''
     When using certain types of projections in Cartopy, you may find that there
-    is not a direct 1-to-1 projection simularity. When looking at the three Lambert
+    is not a direct 1-to-1 projection similarity. When looking at the three Lambert
     projections offered, you will notice the closest match to the NCL projection
     is actually the Lambert Cylindrical projection. This is due to NCL having certain
     "smoothing" and "flattening" options for the Lambert Conformal projection not seen 
@@ -91,11 +96,10 @@ def Plot(row, col, pos, proj, title):
     '''
     
     # Plot data and create colorbar
-    newcmp = gvcmaps.BlueYellowRed
     t.plot.contourf(ax=ax, cmap=newcmp, transform=ccrs.PlateCarree(), 
                     levels = 14, cbar_kwargs={"orientation":"horizontal", 
                     "ticks":np.arange(0, 240, 20),  "label":'', "shrink":0.9})
-
+  
     plt.title(title, loc='center', y=1.17, size=15)
     plt.title(t.units, loc='right', y=1.08,  size=14)
     plt.title("precipitation", loc='left', y=1.08, size=14)
