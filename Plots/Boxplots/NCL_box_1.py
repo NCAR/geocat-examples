8d0c391bf4dc9aecf8edbe4724a0c7ebbf6cd6e3
anissa111 <anissazacharias@gmail.com>
2020-08-17 16:28:28 -0600

    Removed modified file

--- Plots/Boxplots/NCL_box_1.py
+++ Plots/Boxplots/NCL_box_1.py
@@ -28,18 +28,18 @@ np.random.seed(seed)
 
 data = np.random.lognormal(size=(40, 3), mean=1, sigma=.7)
 for a in range(len(data)):
-    data[a] = [x - 4 for x in data[a]]
+    data[a] = [x-4 for x in data[a]]
+
 
 ###############################################################################
 # Plot:
 
 # Create figure and axis
 w = 0.25
-fig, ax = plt.subplots(figsize=(6, 6))
-boxplots = ax.boxplot(data,
-                      labels=['Control', '-2Xna', '2Xna'],
-                      widths=[w, w, w],
-                      showfliers=False)
+fig, ax = plt.subplots(figsize=(6,6))
+boxplots = ax.boxplot(data, labels=['Control', '-2Xna', '2Xna'],
+                      widths=[w, w, w], showfliers=False)
+
 
 # Set whiskers style to dashed
 plt.setp(boxplots['whiskers'], linestyle='--')
@@ -52,14 +52,11 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 
 # Use geocat.viz.util convenience function to set axes tick values
-gvutil.set_axes_limits_and_ticks(ax,
-                                 ylim=(-6.0, 9.0),
+gvutil.set_axes_limits_and_ticks(ax, ylim=(-6.0, 9.0),
                                  yticks=[-3.0, 0.0, 3.0, 6.0])
 
 # Use geocat.viz.util convenience function to add minor and major tick lines
-gvutil.add_major_minor_ticks(ax,
-                             y_minor_per_major=3,
-                             x_minor_per_major=1,
+gvutil.add_major_minor_ticks(ax, y_minor_per_major=3, x_minor_per_major=1,
                              labelsize=14)
 
 # Use geocat.viz.util convenience function to add title to the plot axis.
