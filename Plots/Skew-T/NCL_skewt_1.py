b152417df619acfede249caf6000dfeb85460512
Rose Craker <hcraker@ucar.edu>
2020-08-21 15:53:30 -0400

    Create NCL style Skew-T axes

--- Plots/Skew-T/NCL_skewt_1.py
+++ Plots/Skew-T/NCL_skewt_1.py
@@ -14,8 +14,9 @@ See following URLs to see the reproduced NCL plot & script:
 import matplotlib.pyplot as plt
 import numpy as np
 from metpy.plots import SkewT
+from metpy.units import units
 import geocat.viz.util as gvutil
-
+"""
 ###############################################################################
 # Plot Skew-T with MetPy Defaults:
 ## Note that there are not labels on the axes. This is because we have not yet
@@ -23,9 +24,42 @@ import geocat.viz.util as gvutil
 ## data to create appropriate labels.
 fig = plt.figure(figsize=(9, 9))
 skew = SkewT(fig)
+ax = skew.ax
 
 skew.plot_dry_adiabats()
 skew.plot_moist_adiabats()
 skew.plot_mixing_lines()
+
+gvutil.set_titles_and_labels(ax, maintitle="MetPy Default Skew-T")
+plt.show()
+plt.close()
+"""
+###############################################################################
+# Plot Skew-T that is similar to NCL's default Skew-T plot
+fig = plt.figure(figsize=(9, 9))
+# The rotation keyword changes how skewed the temperature lines are. MetPy has
+# a default skew of 30 degrees
+skew = SkewT(fig, rotation=45)
+ax = skew.ax
+
+# Choose starting temperatures in Kelvin for the dry adiabats
+t0 = units.K * np.arange(253.15, 444.15, 10)
+skew.plot_dry_adiabats(t0=t0, linestyles='solid', colors='goldenrod', linewidths=1.5)
+
+# Choose starting temperatures in Kelvin for the moist adiabats
+t0 = units.K * np.arange(282.15, 306.15, 4)
+skew.plot_moist_adiabats(t0=t0, linestyles='solid', colors='lime', linewidth=1.5)
+
+# Choose mixing ratios
+w = np.array([0.001, 0.002, 0.003, 0.005, 0.008, 0.012, 0.020]).reshape(-1, 1)
+
+# Choose the range of pressures that the mixing ratio lines are drawn over
+p = units.hPa * np.linspace(1000, 400, 7)
+
+# Plot mixing ratio lines
+skew.plot_mixing_lines(w=w, p=p, linestyle='dashed', colors='lime', linewidths=1)
+
+gvutil.set_axes_limits_and_ticks(ax=ax, xlim=[-32, 38], yticks=[1000, 850, 700, 500, 400, 300, 250, 200, 150, 100])
+gvutil.set_titles_and_labels(ax=ax, maintitle="NCL Style Plot")
 
 plt.show()
