9c7b0618b02f9ed8c023fc64e17554f177577634
michaelavs <michaela.sizemore@gmail.com>
2020-08-24 11:32:58 -0600

    Updating push with MPL version

--- Plots/CSV/NCL_write_csv_1.py
+++ Plots/CSV/NCL_write_csv_1.py
@@ -61,6 +61,7 @@ df = pd.DataFrame([x1, x2, x3])
 df = df.T
 
 # Export data frame to csv file
-# setting 'header' and 'index' to False will remove a default row and column
+# setting 'header' and 'index' to False will remove a default crow and column
 # number label 
 df.to_csv('example1b.csv', header=False, index=False)
+
