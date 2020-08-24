f92f542c7c5c2b7941317dd436136eac88e457c9
michaelavs <michaela.sizemore@gmail.com>
2020-08-24 09:50:39 -0600

    Testing MPL update

--- Plots/Polygons/NCL_polyg_19.py
+++ Plots/Polygons/NCL_polyg_19.py
@@ -52,8 +52,7 @@ file11 = open(gdf.get("shape_files/gadm36_PRI_0.shx"), 'r')
 file12 = open(gdf.get("shape_files/gadm36_PRI_0.prj"), 'r')
 
 # Open the text file with the population data
-state_population_file = open(gdf.get("ascii_files/us_state_population.txt"),
-                             'r')
+state_population_file = open(gdf.get("ascii_files/us_state_population.txt"), 'r')
 
 # Open shapefiles
 us = shp.Reader(gdf.get("shape_files/gadm36_USA_1.dbf"))
@@ -63,11 +62,9 @@ pr = shp.Reader(gdf.get("shape_files/gadm36_PRI_0.dbf"))
 ###############################################################################
 # Set colormap data and colormap bounds:
 
-colormap = colors.ListedColormap([
-    'lightpink', 'wheat', 'palegreen', 'powderblue', 'thistle', 'lightcoral',
-    'peru', 'dodgerblue', 'slateblue', 'firebrick', 'sienna', 'olivedrab',
-    'steelblue', 'navy'
-])
+colormap = colors.ListedColormap(['lightpink', 'wheat', 'palegreen', 'powderblue', 'thistle',
+                                  'lightcoral', 'peru', 'dodgerblue', 'slateblue', 'firebrick', 'sienna', 'olivedrab',
+                                  'steelblue', 'navy'])
 
 colorbounds = [0, 1, 2.5, 3, 4, 5, 6, 7, 8, 9, 10, 12, 25, 38, 40]
 
@@ -85,10 +82,9 @@ def getStatePopulations(state_population_file):
         nameandpop = line.split(" ")
         if nameandpop[-1].isnumeric():
             name = nameandpop[0]
-            pop = (int)(nameandpop[-1]) / 1000000
+            pop = (int)(nameandpop[-1])/1000000
             population_dict[name] = pop
     return population_dict
-
 
 ###############################################################################
 # Define helper function to get the color of each state based on its population
@@ -98,14 +94,13 @@ def findDivColor(colorbounds, pdata):
 
     for x in range(len(colorbounds)):
 
-        if pdata >= colorbounds[len(colorbounds) - 1]:
-            return colormap.colors[x - 1]
+        if pdata >= colorbounds[len(colorbounds)-1]:
+            return colormap.colors[x-1]
         if pdata >= colorbounds[x]:
             continue
         else:
             # Index is 'x-1' because colorbounds is one item longer than colormap
-            return colormap.colors[x - 1]
-
+            return colormap.colors[x-1]
 
 ###############################################################################
 # Define helper function to remove ticks from axes
@@ -116,20 +111,19 @@ def removeTicks(axis):
     axis.get_xaxis().set_visible(False)
     axis.get_yaxis().set_visible(False)
 
-
 ###############################################################################
 # Define helper function to plot and color each state
 
 
 def plotPR(region, axis, xlim, puertoRico, waterBody):
-    # Plot each shape within a region (ex. mainland Alaska and all of it's surrounding Alaskan islands)
+# Plot each shape within a region (ex. mainland Alaska and all of it's surrounding Alaskan islands)
     for i in range(len(region.shape.parts)):
 
         i_start = region.shape.parts[i]
-        if i == len(region.shape.parts) - 1:
+        if i == len(region.shape.parts)-1:
             i_end = len(region.shape.points)
         else:
-            i_end = region.shape.parts[i + 1]
+            i_end = region.shape.parts[i+1]
 
         # Create new empty lists to hold lat coordinates, lon coordinates, and filled polygons, or "patches"
         x = []
@@ -149,23 +143,18 @@ def plotPR(region, axis, xlim, puertoRico, waterBody):
         # Plot outline of each region
         axis.plot(x, y, color='black', linewidth=0.1, zorder=1)
 
-        # Fill each state with color:
-        # Determine the key of each region to get the value from the population dictionary
-
-        # Fill each state with color:
-        # Determine the key of each region to get the value from the population dictionary
+# Fill each state with color:
+# Determine the key of each region to get the value from the population dictionary
+   
+    # Fill each state with color:
+    # Determine the key of each region to get the value from the population dictionary
         if puertoRico is True:
             abbrevstate = 'PR'
             pop = population_dict[abbrevstate]
             color = findDivColor(colorbounds, pop)
             # Set characteristics and measurements of each filled polygon "patch"
-            patches.append(
-                Polygon(np.vstack((x, y)).T, True, color=color, linewidth=0.1))
-            pc = PatchCollection(patches,
-                                 match_original=True,
-                                 edgecolor='k',
-                                 linewidths=0.1,
-                                 zorder=2)
+            patches.append(Polygon(np.vstack((x, y)).T, True, color=color, linewidth=0.1))
+            pc = PatchCollection(patches, match_original=True, edgecolor='k', linewidths=0.1, zorder=2)
             # Plot filled region on axis
             axis.add_collection(pc)
 
@@ -178,20 +167,14 @@ def plotPR(region, axis, xlim, puertoRico, waterBody):
         #     pc = PatchCollection(patches, match_original=True, edgecolor='k', linewidths=0.1, zorder=2)
         #     # Plot filled region on axis
         #     axis.add_collection(pc)
-
+            
         # If the region being plotted is a body of water with no population
         else:
             # Set characteristics and measurements of each filled polygon "patch"
-            patches.append(
-                Polygon(np.vstack((x, y)).T, True, color='white', linewidth=.7))
-            pc = PatchCollection(patches,
-                                 match_original=True,
-                                 edgecolor='white',
-                                 linewidth=.8,
-                                 zorder=3)
+            patches.append(Polygon(np.vstack((x, y)).T, True, color='white', linewidth=.7))
+            pc = PatchCollection(patches, match_original=True, edgecolor='white', linewidth=.8, zorder=3)
             # Plot filled region on axis
             axis.add_collection(pc)
-
 
 def plotRegion(region, axis, xlim, puertoRico, waterBody):
 
@@ -199,10 +182,10 @@ def plotRegion(region, axis, xlim, puertoRico, waterBody):
     for i in range(len(region.shape.parts)):
 
         i_start = region.shape.parts[i]
-        if i == len(region.shape.parts) - 1:
+        if i == len(region.shape.parts)-1:
             i_end = len(region.shape.points)
         else:
-            i_end = region.shape.parts[i + 1]
+            i_end = region.shape.parts[i+1]
 
         # Create new empty lists to hold lat coordinates, lon coordinates, and filled polygons, or "patches"
         x = []
@@ -222,12 +205,12 @@ def plotRegion(region, axis, xlim, puertoRico, waterBody):
         # Plot outline of each region
         axis.plot(x, y, color='black', linewidth=0.1, zorder=1)
 
-        # Fill each state with color:
-        # Determine the key of each region to get the value from the population dictionary
-
-        # Fill each state with color:
-        # Determine the key of each region to get the value from the population dictionary
-
+# Fill each state with color:
+# Determine the key of each region to get the value from the population dictionary
+   
+    # Fill each state with color:
+    # Determine the key of each region to get the value from the population dictionary
+        
         if puertoRico is False and waterBody is False:
             abbrevname = shape.record[-1].split(".")
             abbrevstate = abbrevname[1]
@@ -237,40 +220,24 @@ def plotRegion(region, axis, xlim, puertoRico, waterBody):
             pop = population_dict[abbrevstate]
             color = findDivColor(colorbounds, pop)
             # Set characteristics and measurements of each filled polygon "patch"
-            patches.append(
-                Polygon(np.vstack((x, y)).T, True, color=color, linewidth=0.1))
-            pc = PatchCollection(patches,
-                                 match_original=True,
-                                 edgecolor='k',
-                                 linewidths=0.1,
-                                 zorder=2)
+            patches.append(Polygon(np.vstack((x, y)).T, True, color=color, linewidth=0.1))
+            pc = PatchCollection(patches, match_original=True, edgecolor='k', linewidths=0.1, zorder=2)
             # Plot filled region on axis
             axis.add_collection(pc)
         # If the region being plotted is a body of water with no population
         else:
             # Set characteristics and measurements of each filled polygon "patch"
-            patches.append(
-                Polygon(np.vstack((x, y)).T, True, color='white', linewidth=.7))
-            pc = PatchCollection(patches,
-                                 match_original=True,
-                                 edgecolor='white',
-                                 linewidth=.8,
-                                 zorder=3)
+            patches.append(Polygon(np.vstack((x, y)).T, True, color='white', linewidth=.7))
+            pc = PatchCollection(patches, match_original=True, edgecolor='white', linewidth=.8, zorder=3)
             # Plot filled region on axis
             axis.add_collection(pc)
-
 
 ###############################################################################
 # Plot:
 
 # Create figure
 fig = plt.figure(figsize=(8, 8))
-spec = gridspec.GridSpec(ncols=1,
-                         nrows=2,
-                         hspace=0.05,
-                         wspace=0.1,
-                         figure=fig,
-                         height_ratios=[2, 1])
+spec = gridspec.GridSpec(ncols=1, nrows=2, hspace=0.05, wspace=0.1, figure=fig, height_ratios=[2, 1])
 
 # Create upper axis
 ax1 = fig.add_subplot(spec[0, 0], frameon=False)
@@ -297,10 +264,7 @@ for shape in us.shapeRecords():
     if shape.record[3] == 'Alaska':
         plotRegion(shape, axin1, [None, 100], puertoRico=False, waterBody=False)
     elif shape.record[3] == 'Hawaii':
-        plotRegion(shape,
-                   axin2, [-161, None],
-                   puertoRico=False,
-                   waterBody=False)
+        plotRegion(shape, axin2, [-161, None], puertoRico=False, waterBody=False)
     else:
         plotRegion(shape, ax1, [None, None], puertoRico=False, waterBody=False)
 
@@ -315,20 +279,17 @@ for shape in pr.shapeRecords():
 #         plotRegion(shape, ax1, [None, None], puertoRico=False, waterBody=True)
 
 # Set title using helper function from geocat-viz
-title = r"$\bf{Population}$" + " " + r"$\bf{in}$" + " " + r"$\bf{Millions}$" + " " + r"$\bf{(2014)}$"
-gvutil.set_titles_and_labels(ax1, maintitle=title, maintitlefontsize=18)
+title = r"$\bf{Population}$"+" "+r"$\bf{in}$"+" "+r"$\bf{Millions}$"+" "+r"$\bf{(2014)}$"
+gvutil.set_titles_and_labels(ax1, maintitle=title,
+                             maintitlefontsize=18)
 
 # Create fourth inset axis for colorbar
 axin4 = inset_axes(ax2, width="115%", height="12%", loc='center')
 
 # Create colorbar
-cb = fig.colorbar(cm.ScalarMappable(cmap=colormap, norm=norm),
-                  cax=axin4,
-                  boundaries=colorbounds,
-                  ticks=colorbounds[1:-1],
-                  spacing='uniform',
-                  orientation='horizontal')
+cb = fig.colorbar(cm.ScalarMappable(cmap=colormap, norm=norm), cax=axin4, boundaries=colorbounds,
+                  ticks=colorbounds[1:-1], spacing='uniform', orientation='horizontal')
 
 plt.show()
 z = time.perf_counter()
-print('total time', (z - x) / 60)
+print('total time', (z-x)/60)
