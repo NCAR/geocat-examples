ab16dd3ab79bdc3c7ccbe77c88aca2003be6160f
michaelavs <michaela.sizemore@gmail.com>
2020-08-27 09:19:28 -0600

    Updating local and remote versions

--- Plots/Vectors/NCL_vector_3.py
+++ Plots/Vectors/NCL_vector_3.py
@@ -32,15 +32,14 @@ file_in = xr.open_dataset(gdf.get("netcdf_files/uv300.nc"))
 # Read in data from netCDF file.
 # Note that when we extract ``u`` and ``v`` from the file,
 # we only read every third latitude and longitude.
-# This choice was made because ``geocat.viz`` doesn't offer an
-# equivalent function to ncl's ``vcMinDistanceF`` yet.
-ds = file_in.isel(time=1, lon=slice(0, -1, 3), lat=slice(1, -1, 3))
+
+ds = file_in.isel(time=1, lon=slice(0,-1,3), lat=slice(1,-1,3))
 
 ###############################################################################
 # Plot:
 
 # Generate figure (set its size (width, height) in inches)
-plt.subplots(figsize=(10, 5.25))
+plt.subplots(figsize=(10,5.25))
 
 # Generate axes using Cartopy projection
 ax = plt.axes(projection=ccrs.PlateCarree())
@@ -49,48 +48,23 @@ z = gvutil.set_vector_density(ds, 0.017)
 # Draw vector plot
 # Notes
 
-# 1. We plot every third vector in each direction, which is not as nice as vcMinDistanceF in NCL
+# 1. We are using `set_vector_density` on line 47 as a replacement for NCL's vcMinDistanceF
 # 2. There is no matplotlib equivalent to "CurlyVector"
-Q = plt.quiver(z['lon'],
-               z['lat'],
-               z['U'].data,
-               z['V'].data,
-               color='black',
-               zorder=1,
-               pivot="middle",
-               width=0.0007,
-               headwidth=10)
+Q = plt.quiver(z['lon'], z['lat'], z['U'].data, z['V'].data, color='black',
+               zorder=1, pivot="middle", width=0.0007, headwidth=10)
 
 # Draw legend for vector plot
-qk = ax.quiverkey(Q,
-                  167.5,
-                  72.5,
-                  20,
-                  r'20',
-                  labelpos='N',
-                  coordinates='data',
-                  color='black',
-                  zorder=2)
+qk = ax.quiverkey(Q, 167.5, 72.5, 20, r'20', labelpos='N',
+                  coordinates='data', color='black', zorder=2)
 
 # Turn on continent shading
-ax.add_feature(cartopy.feature.LAND,
-               edgecolor='lightgray',
-               facecolor='lightgray',
-               zorder=0)
+ax.add_feature(cartopy.feature.LAND, edgecolor='lightgray', facecolor='lightgray', zorder=0)
 
 # Draw the key for the quiver plot as a rectangle patch
-ax.add_patch(
-    plt.Rectangle((155, 65),
-                  25,
-                  25,
-                  facecolor='white',
-                  edgecolor='black',
-                  zorder=1))
+ax.add_patch(plt.Rectangle((155, 65), 25, 25, facecolor='white', edgecolor='black', zorder=1))
 
 # Use geocat.viz.util convenience function to set axes tick values
-gvutil.set_axes_limits_and_ticks(ax,
-                                 xticks=range(-180, 181, 30),
-                                 yticks=range(-90, 91, 30))
+gvutil.set_axes_limits_and_ticks(ax, xticks=range(-180, 181, 30), yticks=range(-90, 91, 30))
 
 # Use geocat.viz.util convenience function to add minor and major tick lines
 gvutil.add_major_minor_ticks(ax, labelsize=12)
@@ -99,9 +73,7 @@ gvutil.add_major_minor_ticks(ax, labelsize=12)
 gvutil.add_lat_lon_ticklabels(ax)
 
 # Use geocat.viz.util convenience function to add titles to left and right of the plot axis.
-gvutil.set_titles_and_labels(ax,
-                             lefttitle=ds['U'].long_name,
-                             righttitle=ds['U'].units)
+gvutil.set_titles_and_labels(ax, lefttitle=ds['U'].long_name, righttitle=ds['U'].units)
 
 # Add timestamp
 ax.text(-200, -115, f'Created: {datetime.now()}')
