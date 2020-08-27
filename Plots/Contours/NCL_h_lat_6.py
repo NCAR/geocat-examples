411480df5779941c8240f6f7f31228e845ddf5de
Rose Craker <hcraker@ucar.edu>
2020-08-27 11:43:15 -0400

    Created axes and plotted data

--- Plots/Contours/NCL_h_lat_6.py
+++ Plots/Contours/NCL_h_lat_6.py
@@ -30,3 +30,51 @@ from geocat.viz import cmaps as gvcmap
 ds = xr.open_dataset(gdf.get("netcdf_files/mxclim.nc"))
 # Extract variables
 U = ds.U[0, :, :]
+
+################################################################################
+# Plot:
+
+# Generate figure (set its size (width, height) in inches) and axes
+plt.figure(figsize=(8, 8))
+ax = plt.axes()
+
+# Set y-axis to have log-scale
+plt.yscale('log')
+
+# Specify which contours should be drawn
+levels = np.linspace(-55, 55, 23)
+
+# Plot contour lines
+lines = U.plot.contour(ax=ax,
+                       levels=levels,
+                       colors='black',
+                       linewidths=0.5,
+                       linestyles='solid',
+                       add_labels=False)
+
+# Draw contour labels and set their backgrounds to be white
+ax.clabel(lines, fmt='%d', levels=levels)
+[
+    txt.set_bbox(dict(facecolor='white', edgecolor='none', pad=1))
+    for txt in lines.labelTexts
+]
+
+# Plot filled contours
+colors = U.plot.contourf(ax=ax,
+                         levels=levels,
+                         cmap=gvcmap.BlWhRe,
+                         add_labels=False,
+                         add_colorbar=False)
+
+# Use geocat.viz.util convenience function to set axes tick values
+# Set y-lim inorder for y-axis to have descending values
+gvutil.set_axes_limits_and_ticks(ax,
+                                 xticks=np.linspace(-60, 60, 5),
+                                 xticklabels=['60S', '30S', '0', '30N', '60N'],
+                                 ylim=ax.get_ylim()[::-1],
+                                 yticks=U["lev"])
+
+# Change formatter or else we tick values formatted in exponential form
+ax.yaxis.set_major_formatter(ScalarFormatter())
+
+plt.show()
