471749f5250fa1673d443c61d0c7978dfb66d976
Rose Craker <hcraker@ucar.edu>
2020-09-03 10:40:14 -0400

    Added colorbar

--- Plots/Panels/NCL_panel_10.py
+++ Plots/Panels/NCL_panel_10.py
@@ -20,6 +20,7 @@ import xarray as xr
 import cartopy
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 import geocat.datafiles as gdf
 import geocat.viz.util as gvutil
@@ -45,7 +46,19 @@ fig, (ax1, ax2) = plt.subplots(nrows=1,
                                sharey=True,
                                figsize=(12, 9),
                                gridspec_kw=dict(wspace=0,
-                                                width_ratios=[0.75, 0.25]))
+                                                width_ratios=[0.75, 0.25],
+                                                left=0.15,
+                                                right=0.85,
+                                                top=0.85,
+                                                bottom=0.15))
+# Create inset axes for color bar
+cax1 = inset_axes(ax1,
+                  width='100%',
+                  height='7%',
+                  loc='lower left',
+                  bbox_to_anchor=(0, -0.15, 1, 1),
+                  bbox_transform=ax1.transAxes,
+                  borderpad=0)
 
 # Draw contour lines
 ax1.contour(lon,
@@ -62,6 +75,10 @@ cf = ax1.contourf(lon,
                   chi,
                   levels=np.arange(-12, 13, 2),
                   cmap=gvcmaps.BlWhRe)
+
+# Draw colorbar with larger tick labels
+cbar = plt.colorbar(cf, cax=cax1, orientation='horizontal', ticks=np.arange(-10, 11, 2))
+cbar.ax.tick_params(labelsize=12)
 
 # Use geocat.viz.util convenience function to set axes limits & tick values
 gvutil.set_axes_limits_and_ticks(ax1,
@@ -85,5 +102,4 @@ gvutil.set_titles_and_labels(ax1,
                              righttitle="m2/s",
                              ylabel="elapsed time")
 
-plt.tight_layout()
 plt.show()
