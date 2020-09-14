39641b2c01497616ebdd3d8229d78a09e4ffeaae
Heather Craker <hcraker@purdue.edu>
2020-09-14 14:48:28 -0400

    Formatted axes with nested gridspec

--- Plots/Panels/NCL_panel_15.py
+++ Plots/Panels/NCL_panel_15.py
@@ -15,7 +15,7 @@ See following URLs to see the reproduced NCL plot & script:
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import matplotlib.gridspec as gridspec
 import numpy as np
 import xarray as xr
 
@@ -34,4 +34,41 @@ t = ds.T.isel(time=0)
 t_1 = t.isel(z_t=0)
 t_2 = t.isel(z_t=1)
 t_6 = t.isel(z_t=5)
-print(t_1, t_2, t_6)
+
+##############################################################################
+# Plot:
+
+"""
+The outer gridspec will be a 2x2 grid with the top row having a height that
+is two thirds of the figure height. The bottom row will have a height that is
+one third of the figure height. The right column will be just thick enough to
+accommodate the color bars.
+"""
+fig = plt.figure(figsize=(7, 15))
+
+outer_grid = gridspec.GridSpec(nrows=2,
+                               ncols=2,
+                               figure=fig,
+                               height_ratios=[2/3, 1/3],
+                               width_ratios=[0.95, 0.05])
+
+"""
+The inner gridspec will be nested in the top, left cell of the outer gridspec.
+This inner gridspec will have two rows and one column to accommodate the
+stacked plots that will share a colorbar.
+"""
+inner_grid = gridspec.GridSpecFromSubplotSpec(nrows=2,
+                                              ncols=1,
+                                              subplot_spec=outer_grid[0])
+
+# Choose the map projection
+proj = ccrs.PlateCarree()
+
+# Add the subplots
+ax1 = fig.add_subplot(inner_grid[0], projection=proj) # upper cell of inner grid
+ax2 = fig.add_subplot(inner_grid[1], projection=proj) # lower cell of inner grid
+cax1 = fig.add_subplot(outer_grid[1]) # upper right cell of outer grid, for top color bar
+ax3 = fig.add_subplot(outer_grid[2], projection=proj) # bottom left cell of outer grid
+cax1 = fig.add_subplot(outer_grid[3]) # bottom right cell of outer grid, for bottom color bar
+
+plt.show()
