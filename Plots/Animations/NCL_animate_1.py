a81a93c78d931a24b40fe2bb5aba99989c820be2
michaelavs <michaela.sizemore@gmail.com>
2021-02-19 13:27:06 -0700

    Updating code and conf.py to allow display of animation

--- Plots/Animations/NCL_animate_1.py
+++ Plots/Animations/NCL_animate_1.py
@@ -2,29 +2,29 @@
 NCL_animate_1.py
 ================
 This script illustrates the following concepts:
-   - Creating animations
+   - Creating animations using matplotlib.FuncAnimation
 
 See following URLs to see the reproduced NCL plot & script:
     - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/animate_1.ncl
     - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/animate_1_1_lg.png
 
 Please note:
-    This code will create and save a .gif file locally. The .gif file will be
-    located in the same directory used as your working directory. 
+    Executing this script will not display a gif, but you have the option to
+    uncomment a line at the bottom that will save a gif in the same directory
+    as this script.
 """
 ###############################################################################
+
 # Import packages:
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from matplotlib import pyplot as plt
 import numpy as np
 import xarray as xr
-from PIL import Image
-
-import glob
-from IPython.display import display
+import matplotlib.animation as animation
 
 import geocat.datafiles as gdf
+import geocat.viz.util as gvutil
 
 ###############################################################################
 # Read in data:
@@ -35,57 +35,68 @@ ds = xr.open_dataset(gdf.get("netcdf_files/meccatemp.cdf"), decode_times=False)
 
 tas = ds.t
 
-# Create empty data array
-frames = []
-
 ###############################################################################
-# Generate projections
-for i in range(30):
-       
-    # Set figure size
-    figsize=(10,20)
-   
-    # Generate axes using Cartopy and draw coastlines
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.coastlines(linewidths=0.5)
-    ax.add_feature(cfeature.LAND, facecolor="lightgray")
-    
-    # Set extent to include latitudes from 34 to 52 and longitudes from 128
-    # to 144
-    ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
-   
-    # Plot data
-    tas[i,:,:].plot.contourf(
-      ax=ax,
-      transform=ccrs.PlateCarree(),
-      vmin=195,
-      vmax=328,
-      levels=53,
-      cmap="inferno",
-      cbar_kwargs={
+# Create animation:
+
+fig = plt.figure(figsize=(10, 8))
+# Generate axes using Cartopy and draw coastlines
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=150))
+ax.coastlines(linewidths=0.5)
+
+# Set extent to include latitudes from 34 to 52 and longitudes from 128
+# to 144
+ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
+
+# Use geocat.viz.util convenience function to set axes limits & tick values
+gvutil.set_axes_limits_and_ticks(ax,
+                                 xlim=(-180, 180),
+                                 ylim=(-90, 90),
+                                 xticks=np.linspace(-180, 180, 13),
+                                 yticks=np.linspace(-90, 90, 7))
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(ax, labelsize=10)
+
+# Use geocat.viz.util convenience function to make latitude, longitude tick labels
+gvutil.add_lat_lon_ticklabels(ax)
+
+# create initial plot that establishes a colorbar
+tas[0, :, :].plot.contourf(
+    ax=ax,
+    transform=ccrs.PlateCarree(),
+    vmin=195,
+    vmax=328,
+    levels=53,
+    cmap="inferno",
+    cbar_kwargs={
         "extendrect": True,
         "orientation": "horizontal",
         "ticks": np.arange(195, 332, 9),
-        "label": "", "shrink":0.90})
-    plt.title("January Global Surface Temperature (K) - Day  " + str(tas.coords['time'].values[i])[:13])
-    plt.savefig(f"Python_Animation_frame_{i:04}.png")
-    # plt.show()
-    plt.close()
-   
-# Gather and sort the png images 
-imgs = sorted(glob.glob("Python_Animation_frame_00*.png"))
+        "label": "", "shrink": 0.90}
+)
 
-# Load each png image into the empty data array in order of time
-for i in imgs:
-    new_frame = Image.open(i)
-    frames.append(new_frame)
 
-# Save into a GIF file that loops forever
-frames[0].save('png_to_gif.gif', format='GIF',
-                append_images=frames[:],
-                save_all=True,
-                duration=300, loop=0)
+# animate function for matplotlib FuncAnimation
+def animate(i):
+    tas[i, :, :].plot.contourf(
+        ax=ax,
+        transform=ccrs.PlateCarree(),
+        vmin=195,
+        vmax=328,
+        levels=53,
+        cmap="inferno",
+        add_colorbar=False,
+    )
 
-# im = Image.open('Python_Animation_frame_0000.png', 'r')
-im =  Image.open('png_to_gif.gif')
-display(im)
+    gvutil.set_titles_and_labels(ax,
+                                 maintitle="January Global Surface Temperature (K) - Day  " + str(
+                                     tas.coords['time'].values[i])[:13],
+                                 xlabel="",
+                                 ylabel="")
+
+
+# runs the animation initiated with the frame from init and progressed with the animate function
+anim = animation.FuncAnimation(fig, animate, frames=30, interval=200)
+
+# Uncomment this line to save the created animation
+anim.save('animate_1.gif', writer='pillow', fps=5)
