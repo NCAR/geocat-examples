4bbdb592a5d66e3ffc77b29c8400dbb03af26781
erinlincoln <erin_lincoln@brown.edu>
2021-06-07 15:04:03 -0600

    Fixed to match how system displays visualizations

--- Plots/Bar/NCL_bar_5.py
+++ Plots/Bar/NCL_bar_5.py
@@ -50,31 +50,26 @@ for n in np.arange(0, num_months, 1):
 # in inches of the bottom left corner of year graph
 warm = [1951, 1953, 1957, 1963, 1965, 1969, 1972, 1976, 1982, 1987, 1991]
 heights_list = [
-    0.52, 0.764, 1.0, 1.25, 1.5135, 1.721, 2.0, 2.215, 2.5345, 2.72, 2.98
+    0.0585, 0.128, 0.195, .267, .342, .4, 0.482, .541, .636, .686, .763
 ]
 
+# Set the number of subplots
+num_subplots = 11
+fig_height = (num_subplots)
 
-# A helper function to create a figure with multiple axes, each of which will have a different bar graph
-def create_figure(width, num_subplots, heights_list):
-    # Create global variables for the figures and an empty dictionary to store the axes
-    global fig, ax0, ax_dict
-    ax_dict = {}
+# Create figure and default axis which will be visible
+fig, ax0 = plt.subplots(figsize=(8, fig_height + 2), constrained_layout=True)
 
-    # Create figure and default axis which will be visible
-    fig = plt.figure(figsize=(width + 2, (num_subplots / 4) + 2))
-    ax0 = fig.add_axes([0.5, 0.5, width, num_subplots / 4])
+# Loop through heights in heights_list and create a different axis in ax_dict
+# at each y height
+i = 1
+ax_dict = {}
+for height in heights_list:
+    ax_dict["ax" + str(i)] = fig.add_axes([0.003, height, 0.984, 0.0658])
+    i = i + 1
 
-    # Loop through heights in heights_list and create a different axis in ax_dict
-    # for each one
-    i = 1
-    for height in heights_list:
-        ax_dict["ax" + str(i)] = fig.add_axes(
-            [0.355, height, width + .295, 0.25 * .75])
-        i = i + 1
-
-
-# A helper function to create a bar chart for each axis
-def bar_plot(ax_name, year):
+# Loop through the axes created in ax_dict and years in warm to make a bar plot for each set
+for ax, year in zip(ax_dict, warm):
     # Find year limits of each bar chart
     year_start = year - 1
     year_end = year + 3
@@ -84,52 +79,18 @@ def bar_plot(ax_name, year):
     year_iend = int(np.where(np.round(date_frac, 3) == year_end)[0])
 
     # Create each bar chart where it is red if it is above 0 and blue if below
-    ax_dict[ax_name].bar(date_frac[year_istart:year_iend],
-                         sst[year_istart:year_iend],
-                         align='edge',
-                         edgecolor='black',
-                         color=[
-                             'red' if (value > 0) else 'blue'
-                             for value in sst[year_istart:year_iend]
-                         ],
-                         width=.08,
-                         linewidth=2)
+    ax_dict[ax].bar(date_frac[year_istart:year_iend],
+                    sst[year_istart:year_iend],
+                    align='edge',
+                    edgecolor='black',
+                    color=[
+                        'red' if (value > 0) else 'blue'
+                        for value in sst[year_istart:year_iend]
+                    ],
+                    width=.08,
+                    linewidth=1)
     # Turn off axis so it is not visible
-    ax_dict[ax_name].axis("off")
-
-
-# A helper function to make the left y axis labels
-def left_tick_labels(num_plots):
-    tick_list = []
-    i = 0
-    tick_list.append("")
-    while i < num_plots:
-        tick_list.append("-1.0")
-        tick_list.append("0.0")
-        tick_list.append("1.0")
-        tick_list.append("")
-        i = i + 1
-    tick_list.append("")
-    return tick_list
-
-
-# A helper function to make the right y axis labels
-def right_tick_labels(year_list):
-    tick_list = []
-    tick_list.append("")
-    for year in year_list:
-        tick_list.append("")
-        tick_list.append(year)
-        tick_list.append("")
-        tick_list.append("")
-    tick_list.append("")
-    return tick_list
-
-
-# Create axes and bar charts by looping through axes dictionary and warm years
-create_figure(3, 11, heights_list)
-for ax, year in zip(ax_dict, warm):
-    bar_plot(ax, year)
+    ax_dict[ax].axis("off")
 
 # Create right y axis by cloning the left side
 axRHS = ax0.twinx()
@@ -140,18 +101,36 @@ degree = u"\u00b0"
 # Use geocat.viz.util convenience function to add titles to the center and right of the plot axis
 gvutil.set_titles_and_labels(ax0,
                              maintitle="Monthly SST Anomalies for Nino-3",
-                             maintitlefontsize=50,
+                             maintitlefontsize=25,
                              righttitle=("(" + degree + "C)"),
-                             righttitlefontsize=39)
+                             righttitlefontsize=18)
 # Add center title
-ax0.text(0.38, 1.05, 'Warm Events', fontsize=38, transform=ax0.transAxes)
+ax0.text(0.38, 1.05, 'Warm Events', fontsize=18, transform=ax0.transAxes)
 
 # Use geocat.viz.util convenience function to add major tick lines
 gvutil.add_major_minor_ticks(ax0, x_minor_per_major=1, y_minor_per_major=1)
 
-# Create the lists for the left and right axes
-left_y_ticks = left_tick_labels(11)
-right_y_ticks = right_tick_labels(11, warm)
+# Create the tick labels for the left axis
+left_y_ticks = []
+i = 0
+left_y_ticks.append("")
+while i < num_subplots:
+    left_y_ticks.append("-1.0")
+    left_y_ticks.append("0.0")
+    left_y_ticks.append("1.0")
+    left_y_ticks.append("")
+    i = i + 1
+left_y_ticks.append("")
+
+# Create the tick labels for the right axis
+right_y_ticks = []
+right_y_ticks.append("")
+for year in warm:
+    right_y_ticks.append("")
+    right_y_ticks.append(year)
+    right_y_ticks.append("")
+    right_y_ticks.append("")
+right_y_ticks.append("")
 
 # Use geocat.viz.util convenience function to set axes limits & tick values without calling several matplotlib functions
 gvutil.set_axes_limits_and_ticks(
@@ -161,17 +140,20 @@ gvutil.set_axes_limits_and_ticks(
     xticklabels=["Jan₋₁", "Jul₋₁", "Jan₀", "Jan₀", "Jan₊₁", "Jul₊₁", "Jan₊₂"],
     yticks=np.linspace(0, 46, 46),
     yticklabels=left_y_ticks)
+
+ax0.spines['right'].set_visible(False)
 gvutil.set_axes_limits_and_ticks(axRHS,
-                                 yticks=np.linspace(0, 46, 46),
+                                 ylim=(0, 45),
+                                 yticks=np.linspace(0, 45, 46),
                                  yticklabels=right_y_ticks)
 
 # Set tick parameters for all axes
-ax0.tick_params(axis="x", size=28, labelsize=30)
-ax0.tick_params(axis="y", size=14, labelsize=20)
-axRHS.tick_params(axis="y", size=14, labelsize=20)
+ax0.tick_params(axis="x", length=9, labelsize=12)
+ax0.tick_params(axis="y", length=5, labelsize=8)
+axRHS.tick_params(axis="y", length=6, labelsize=8)
 
 # Show plot
-plt.show()
+plt.plot()
 
 ###############################################################################
 # Although the code above does recreate the NCL plot, it is tedious to replicate
@@ -183,8 +165,8 @@ plt.show()
 ###############################################################################
 # Plot utilizing the subplot method:
 
-# Create the figure with figure size (height, width) in inches
-fig2, ax2 = plt.subplots(11, 1, figsize=(12, 24))
+# Create the figure with figure size (width, height) in inches
+fig2, ax2 = plt.subplots(11, 1, figsize=(6, 8))
 
 # Loop through all of the years and plot them as a bar chart
 i = 0
@@ -224,12 +206,17 @@ for year in warm:
 
     # Create right hand side axis, add the year as a title to the axis, and remove labels
     ax2RHS = ax2[i].twinx()
-    ax2RHS.set_ylabel(str(year), size=14, fontweight='bold')
+    ax2RHS.set_ylabel(str(year), size=8, fontweight='bold')
     ax2RHS.set_yticklabels([])
+
+    # Set the tick and tick label size for both axes on all subplots
+    ax2[i].tick_params(axis="x", size=5, labelsize=5)
+    ax2[i].tick_params(axis="y", size=7, labelsize=6)
+
     i = i + 1
 
 # Adjust the white space between each subplot
-plt.subplots_adjust(hspace=.4)
+plt.subplots_adjust(hspace=1)
 
 # Make a variable for the degree symbol
 degree = u"\u00b0"
@@ -238,11 +225,11 @@ degree = u"\u00b0"
 fig2.suptitle("Monthly SST Anomalies for Nino-3",
               horizontalalignment='center',
               y=0.93,
-              fontsize=26,
+              fontsize=15,
               fontweight='bold')
 
 # Add subtitles
-fig2.text(0.42, .9, 'Warm Events', fontsize=24)
-fig2.text(0.12, .9, "(" + degree + "C)", fontsize=24)
+fig2.text(0.42, .895, 'Warm Events', fontsize=9)
+fig2.text(0.12, .895, "(" + degree + "C)", fontsize=9)
 
 plt.show()
