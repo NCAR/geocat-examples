73b66117b1b4d3ab65fdb28af4614a98c4aa9a6b
Jiaqi Li <jiaqili1620@gmail.com>
2021-06-09 07:31:11 -0600

    inital commit

---
+++ Plots/Line/NCL_text_add_1.py
@@ -0,0 +1,66 @@
+"""
+NCL_text_add_1.py
+===============
+This script illustrates the following concepts:
+   - Adding text to a plot using plot data coordinates
+   - Decreasing the font size of text
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/text_add_1.ncl
+    - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/text_1_lg.png
+"""
+
+###############################################################################
+# Import packages:
+
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+import geocat.datafiles as gdf
+from geocat.viz import util as gvutil
+
+###############################################################################
+# Read in data:
+
+# Open a netCDF data file using xarray default engine and load the data into xarrays
+ds = xr.open_dataset(gdf.get("netcdf_files/uv300.nc"))
+# Extract variables
+uz = ds.U.isel(time=0, lon=8)
+lon = ds.U.isel(time=0, lon=8).lat
+
+###############################################################################
+# Plot:
+
+# Generate figure (set its size (width, height) in inches) and axes
+plt.figure(figsize=(6.5, 6.5))
+ax = plt.gca()
+
+# Plot data
+plt.plot(lon, uz.values, c='gray')
+
+# Add text with set parameters
+text_kwargs = dict(ha='center', va='center', fontsize=22, color='black')
+
+plt.text(10, 0.0, 'Text in Plot Coordinates', **text_kwargs)
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(ax,
+                             x_minor_per_major=3,
+                             y_minor_per_major=5,
+                             labelsize=12)
+
+# Use geocat.viz.util convenience function to set axes parameters without calling several matplotlib functions
+# Set axes limits, tick values, and tick labels to show latitude & longitude (i.e. North (N) - South (S))
+gvutil.set_axes_limits_and_ticks(
+    ax,
+    xlim=(-90, 90),
+    ylim=(-10, 40),
+    xticks=np.linspace(-90, 90, 7),
+    xticklabels=['90S', '60S', '30S', '0', '30N', '60N', '90N'])
+
+# Use geocat.viz.util convenience function to set titles and labels
+gvutil.set_titles_and_labels(ax, maintitle='', ylabel='Zonal Wind')
+
+# Show the plot
+plt.show()
