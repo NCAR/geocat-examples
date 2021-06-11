3ad952df27cf9a11ef89417d32e3358374db6d83
Jiaqi Li <jiaqili1620@gmail.com>
2021-06-11 12:52:06 -0600

    modify plot with outlines

--- Plots/Bar/NCL_bar_1.py
+++ Plots/Bar/NCL_bar_1.py
@@ -1,6 +1,6 @@
 """
 NCL_bar_1.py
-===============
+============
 This script illustrates the following concepts:
    - Drawing bars instead of curves in an XY plot
    - Changing the aspect ratio of a bar plot
@@ -29,7 +29,7 @@ from geocat.viz import util as gvutil
 ds = xr.open_dataset(gdf.get("netcdf_files/soi.nc"))
 date = ds.date
 dsoik = ds.DSOI_KET
-dsoid = ds.DSOI_DEC
+#dsoid = ds.DSOI_DEC
 
 # Dates in the file are represented by year and month (YYYYMM)
 # representing them fractionally will make plotting the data easier
@@ -48,7 +48,7 @@ for n in np.arange(0, num_months, 1):
 fig = plt.figure(constrained_layout=True, figsize=(7, 7))
 axes = fig.subplots(nrows=3)
 
-# Set baseline as the minimum excluding the invalid values in the array
+# Set baseline as the minimum of the array, excluding the invalid values
 baseline = np.nanmin(dsoik[::8])
 
 # Create barplot
@@ -65,7 +65,7 @@ axes[0].bar(date_frac[::8],
 gvutil.add_major_minor_ticks(axes[0],
                              x_minor_per_major=4,
                              y_minor_per_major=5,
-                             labelsize=12)
+                             labelsize=10)
 
 # Use geocat.viz.util convenience function to set axes parameters
 gvutil.set_axes_limits_and_ticks(axes[0],
@@ -76,12 +76,40 @@ gvutil.set_axes_limits_and_ticks(axes[0],
 # Use geocat.viz.util convenience function to set titles and labels
 gvutil.set_titles_and_labels(axes[0], maintitle="Bar plot")
 
-# Create barplot
-axes[1].bar(
-    date_frac[::8],
-    dsoik[::8],
-    color='none',
-)
+# Set offset
+offset = (date_frac[::8][1] - date_frac[::8][0]) / 2
+
+# Create barplot with outline
+xs = [date_frac[::8][0] - offset]
+ys = [0]
+for i in range(len(date_frac[::8])):
+    xs.append(date_frac[::8][i] - offset)
+    xs.append(date_frac[::8][i] + offset)
+    ys.append(dsoik[::8][i])
+    ys.append(dsoik[::8][i])
+
+xs.append(date_frac[::8][-1] + offset)
+ys.append(0)
+
+axes[1].plot(xs, ys, color="black", linewidth=0.5)
+
+# Color the areas within the barplot to only show the outline
+axes[1].fill_between(xs, 0, ys, color='white')
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.add_major_minor_ticks(axes[1],
+                             x_minor_per_major=4,
+                             y_minor_per_major=5,
+                             labelsize=10)
+
+# Use geocat.viz.util convenience function to set axes parameters
+gvutil.set_axes_limits_and_ticks(axes[1],
+                                 yticks=np.arange(-2.0, 2.0, 0.5),
+                                 xlim=(1880, 2000),
+                                 xticks=np.linspace(1880, 2000, 7))
+
+# Use geocat.viz.util convenience function to set titles and labels
+gvutil.set_titles_and_labels(axes[1], maintitle="Bar plot with outlines")
 
 # Create barplot
 axes[2].bar(date_frac[::8],
@@ -96,7 +124,7 @@ axes[2].bar(date_frac[::8],
 gvutil.add_major_minor_ticks(axes[2],
                              x_minor_per_major=4,
                              y_minor_per_major=5,
-                             labelsize=12)
+                             labelsize=10)
 
 # Use geocat.viz.util convenience function to set axes parameters
 gvutil.set_axes_limits_and_ticks(axes[2],
