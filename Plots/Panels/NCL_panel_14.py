627a74146e0123ef43be4d526c711d0712eb203e
Jiaqi Li <jiaqili1620@gmail.com>
2021-06-14 12:29:13 -0600

    initial commit

---
+++ Plots/Panels/NCL_panel_14.py
@@ -0,0 +1,58 @@
+"""
+NCL_panel_14.py
+===============
+This script illustrates the following concepts:
+   - Combining two sets of paneled plots on one page
+   - Adding a common labelbar to paneled plots
+   - Reversing the Y axis
+
+See following URLs to see the reproduced NCL plot & script:
+    - Original NCL script: http://www.ncl.ucar.edu/Applications/Scripts/panel_14.ncl
+    - Original NCL plot: http://www.ncl.ucar.edu/Applications/Images/panel_14_lg.png
+"""
+
+##############################################################################
+# Import packages:
+
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import xarray as xr
+
+import geocat.datafiles as gdf
+import geocat.viz.util as gvutil
+
+##############################################################################
+# Read in data:
+
+# Open a netCDF data file using xarray default engine and load the data into
+# xarrays
+ds = xr.open_dataset(gdf.get("netcdf_files/h_avg_Y0191_D000.00.nc"),
+                     decode_times=False)
+
+##############################################################################
+# Plot:
+fig = plt.figure(figsize=(10, 10))
+
+grid = gridspec.GridSpec(nrows=2, ncols=2, figure=fig)
+
+# Choose the map projection
+proj = ccrs.PlateCarree()
+
+# Add the subplots
+ax1 = fig.add_subplot(grid[0])  # upper left cell of grid
+ax2 = fig.add_subplot(grid[1])  # upper right cell of grid
+ax3 = fig.add_subplot(grid[2], projection=proj)  # lower left cell of grid
+ax4 = fig.add_subplot(grid[3], projection=proj)  # lower right cell of grid
+
+# Plot xy data at upper left and right plots
+ax1.plot(ds.T.isel(time=0, z_t=0).sel(lat_t=slice(30), lon_t=slice(180)),
+         ds.T.z_t,
+         c='black',
+         linewidth=0.5)
+ax2.plot(ds.T.isel(time=0, z_t=0).isel(lat_t=slice(-30), lon_t=slice(180)),
+         ds.T.z_t,
+         c='black',
+         linewidth=0.5)
