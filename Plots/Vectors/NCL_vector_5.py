8092af1ca31656f0a7a7325be41566d870624b3e
Jiaqi Li <jiaqili1620@gmail.com>
2021-06-23 12:21:40 -0600

    readdy for review

--- Plots/Vectors/NCL_vector_5.py
+++ Plots/Vectors/NCL_vector_5.py
@@ -23,7 +23,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 import geocat.datafiles as gdf
-from geocat.viz import cmaps as gvcmaps
 from geocat.viz import util as gvutil
 from geocat.comp import interp_hybrid_to_pressure
 
@@ -39,6 +38,7 @@ P0mb = 1000.
 hyam = ds.hyam  # get a coefficiants
 hybm = ds.hybm  # get b coefficiants
 PS = ds.PS  # get pressure
+PS = PS / 100  # Convert from Pascal to mb
 
 # Read in variables from data interpolated to pressure levels
 # interp_hybrid_to_pressure is the Python version of vinth2p in NCL script
@@ -65,21 +65,22 @@ V = interp_hybrid_to_pressure(data=ds.V,
                               method='log')
 
 # Extract data
-t = T.isel(time=0).sel(lon=170, method="nearest")
-w = W.isel(time=0).sel(lon=170, method="nearest")
-v = V.isel(time=0).sel(lon=170, method="nearest")
+T = T.isel(time=0).sel(lon=170, method="nearest")
+W = W.isel(time=0).sel(lon=170, method="nearest")
+V = V.isel(time=0).sel(lon=170, method="nearest")
 
+# Scale W
 wscaler = np.mean(W)
 vscaler = np.mean(V)
 scale = abs(vscaler / wscaler)
 
-wscale = w * scale
+wscale = W * scale
 
 ###############################################################################
 # Plot:
 
 # Generate figure (set its size (width, height) in inches)
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(10, 11))
 
 # Generate axes using Cartopy and draw coastlines
 ax = plt.axes()
@@ -88,7 +89,7 @@ ax = plt.axes()
 levels = np.linspace(200, 300, 11)
 
 # # Plot contour lines
-t.plot.contour(ax=ax,
+T.plot.contour(ax=ax,
                levels=levels,
                colors='black',
                linewidths=0.5,
@@ -96,43 +97,95 @@ t.plot.contour(ax=ax,
                add_labels=False)
 
 # # Plot filled contours
-colors = t.plot.contourf(ax=ax,
+colors = T.plot.contourf(ax=ax,
                          levels=levels,
-                         cmap='magma',
+                         cmap='viridis',
                          add_labels=False,
                          add_colorbar=False)
 
+# Draw vector plot
+# (there is no matplotlib equivalent to "CurlyVector" yet)
+# Setting the scale parameter to adjust length of the arrows
+Q = ax.quiver(T['lat'],
+              T['plev'],
+              V.data,
+              wscale.data,
+              color='black',
+              zorder=1,
+              pivot="middle",
+              width=0.001,
+              scale=450,
+              angles='xy')
+
+# Draw legend for vector plot
+ax.add_patch(
+    plt.Rectangle((53, 935),
+                  35,
+                  50,
+                  facecolor='white',
+                  edgecolor='black',
+                  clip_on=False))
+qk = ax.quiverkey(Q,
+                  0.835,
+                  0.086,
+                  30,
+                  'Reference Vector',
+                  labelpos='S',
+                  coordinates='figure',
+                  color='black',
+                  fontproperties={'size': 13})
+
+# Add a colorbar
+cax = plt.axes((0.1, 0.0001, 0.8, 0.04))
+plt.colorbar(colors,
+             ax=ax,
+             cax=cax,
+             orientation='horizontal',
+             ticks=levels[:-2:2],
+             extendrect=True,
+             drawedges=True,
+             pad=1)
+
 # Use geocat.viz.util convenience function to add minor and major tick lines
-gvutil.add_major_minor_ticks(ax, labelsize=16)
+gvutil.add_major_minor_ticks(ax, x_minor_per_major=3, labelsize=16)
 
 # Use geocat.viz.util convenience function to set axes tick values
 gvutil.set_axes_limits_and_ticks(ax,
-                                 xlim=(-90, 90),
+                                 xlim=(-88, 88),
                                  ylim=(900, 200),
                                  xticks=np.arange(-60, 61, 30),
                                  yticks=np.array(
                                      [200, 250, 300, 400, 500, 700, 850]),
                                  xticklabels=['60S', '30S', '0', '30N', '60N'])
 
-# Turn off minor ticks on Y axis
-ax.tick_params(axis='y', which='minor', left=False)
+# Use geocat.viz.util convenience function to add titles and the pressure label
+gvutil.set_titles_and_labels(ax,
+                             maintitle="Pressure/Height Vector",
+                             maintitlefontsize=22,
+                             ylabel='Pressure (mb)',
+                             labelfontsize=20)
 
-# Draw vector plot
-# (there is no matplotlib equivalent to "CurlyVector" yet)
-Q = plt.quiver(t['plev'],
-               t['lat'],
-               v.data,
-               wscale.data,
-               color='black',
-               zorder=1,
-               pivot="middle",
-               width=0.001)
+# Create second y-axis to show geo-potential height. Currently we're using
+# arbitrary values for height as we haven't figured out how to make this work
+# properly yet.
+axRHS = ax.twinx()
 
-# # Draw legend for vector plot
-# ax.add_patch(
-#     plt.Rectangle((150, -140),
-#                   30,
-#                   30,
-#                   facecolor='white',
-#                   edgecolor='black',
-#                   clip_on=False))
+# Use geocat.viz.util convenience function to set axes tick values
+gvutil.set_axes_limits_and_ticks(axRHS, ylim=(0, 13), yticks=np.array([4, 8]))
+
+# manually set tick length, width and ticklabel size
+axRHS.tick_params(labelsize=16, length=8, width=0.9)
+
+# Use geocat.viz.util convenience function to add titles and the pressure label
+axRHS.set_ylabel(ylabel='Height (km)', labelpad=10, fontsize=20)
+
+# Force the plot to be square by setting the aspect ratio to 1
+ax.set_box_aspect(1)
+axRHS.set_box_aspect(1)
+
+# Turn off minor ticks on Y axis on the left hand side
+ax.tick_params(axis='y', which='minor', left=False, right=False)
+
+# Show plot
+plt.tight_layout()
+plt.show()
