f791334c35d9bb4d24677cd07c5f7f13bacf3656
Jiaqi Li <jiaqili1620@gmail.com>
2021-07-12 13:34:56 -0600

    without vectors

--- Plots/Contours/NCL_polar_8.py
+++ Plots/Contours/NCL_polar_8.py
@@ -6,7 +6,7 @@ This script illustrates the following concepts:
     - Drawing the northern hemisphere of a polar stereographic map
 
 See following URLs to see the reproduced NCL plot & script:
-    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/polar_8_lg.ncl
+    - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/polar_8.ncl
     - Original NCL plot: https://www.ncl.ucar.edu/Applications/Images/polar_8_lg.png
 """
 ###############################################################################
@@ -26,51 +26,55 @@ from geocat.viz import util as gvutil
 # Read in data:
 
 # Open a netCDF data file using xarray default engine and load the data into xarrays
-ds = xr.open_dataset(gdf.get("netcdf_files/atmos.nc"))
+ds = xr.open_dataset(gdf.get("netcdf_files/atmos.nc"), decode_times=False)
 u = ds.U[0, 1, :, :]
 v = ds.V[0, 1, :, :]
 t = ds.TS[0, :, :]
 
 # Extract slices of data
+U = u.sel(lat=slice(60, 90))
+V = u.sel(lat=slice(60, 90))
+T = t.sel(lat=slice(60, 90))
 
 ###############################################################################
 # Fix the artifact of not-shown-data around 0 and 360-degree longitudes
-wrap_U = gvutil.xr_add_cyclic_longitudes(u, "lon")
-wrap_V = gvutil.xr_add_cyclic_longitudes(v, "lon")
-wrap_T = gvutil.xr_add_cyclic_longitudes(t, "lon")
+wrap_U = gvutil.xr_add_cyclic_longitudes(U, "lon")
+wrap_V = gvutil.xr_add_cyclic_longitudes(V, "lon")
+wrap_T = gvutil.xr_add_cyclic_longitudes(T, "lon")
 
 ###############################################################################
 # Plot:
 
-# Generate axes, using Cartopy, drawing coastlines, and adding features
+# Generate axes with Cartopy projections
 fig = plt.figure(figsize=(10, 12))
 projection = ccrs.NorthPolarStereo()
 ax = plt.axes(projection=projection)
-ax.add_feature(cfeature.LAND, facecolor='lightgray')
+
+# Use Cartopy to add land feature
+land_110m = cfeature.NaturalEarthFeature('physical', 'land', '110m')
+ax.add_feature(land_110m, facecolor='white', edgecolor='black')
 
 # Set map boundary to include latitudes between 0 and 40 and longitudes
 # between -180 and 180 only
-gvutil.set_map_boundary(ax, [-180, 180], [0, 40], south_pad=1)
+gvutil.set_map_boundary(ax, [-180, 180], [60, 90], south_pad=1)
 
-# Set draw_labels to False so that you can manually manipulate it later
+# Set draw_labels to False to manually set labels later
 gl = ax.gridlines(ccrs.PlateCarree(),
                   draw_labels=False,
-                  linestyle="--",
+                  linestyle=(0, (4, 10)),
                   color='black')
 
 # Manipulate latitude and longitude gridline numbers and spacing
-gl.ylocator = mticker.FixedLocator(np.arange(0, 90, 15))
+gl.ylocator = mticker.FixedLocator(np.arange(60, 90, 15))
 gl.xlocator = mticker.FixedLocator(np.arange(-180, 180, 30))
 
 # Manipulate longitude labels (0, 30 E, 60 E, ..., 30 W, etc.)
-ticks = np.arange(0, 210, 30)
-etick = ['0'] + [
-    r'%dE' % tick for tick in ticks if (tick != 0) & (tick != 180)
-] + ['180']
-wtick = [r'%dW' % tick for tick in ticks if (tick != 0) & (tick != 180)]
+ticks = np.arange(30, 151, 30)
+etick = ['0'] + [r'%dE' % tick for tick in ticks] + ['180']
+wtick = [r'%dW' % tick for tick in ticks]
 labels = etick + wtick
-xticks = np.arange(0, 360, 30)
-yticks = np.full_like(xticks, -5)  # Latitude where the labels will be drawn
+xticks = np.arange(0, 360, 30)  # Longitude of the labels
+yticks = np.full_like(xticks, 58)  # Latitude of the labels
 
 for xtick, ytick, label in zip(xticks, yticks, labels):
     if label == '180':
@@ -98,20 +102,51 @@ for xtick, ytick, label in zip(xticks, yticks, labels):
                 verticalalignment='center',
                 transform=ccrs.Geodetic())
 
-# Contour-plot U-data
-p = wrap_U.plot.contour(ax=ax,
-                        vmin=-8,
-                        vmax=16,
-                        transform=ccrs.PlateCarree(),
-                        levels=np.arange(-12, 44, 4),
-                        linewidths=0.5,
-                        cmap='black',
-                        add_labels=False)
+# Set contour levels
+levels = np.arange(249, 283, 3)
+
+# Contourf-plot T-data
+p = wrap_T.plot.contourf(ax=ax,
+                         alpha=0.85,
+                         transform=ccrs.PlateCarree(),
+                         levels=levels,
+                         cmap='viridis',
+                         add_labels=False,
+                         add_colorbar=False,
+                         zorder=3)
+
+# Draw vector plot
+# (there is no matplotlib equivalent to "CurlyVector" yet)
+Q = ax.quiver(wrap_T['lon'],
+              wrap_T['lat'],
+              wrap_U.data,
+              wrap_V.data,
+              zorder=4,
+              pivot="middle",
+              width=0.001,
+              transform=ccrs.PlateCarree())
+
+# Add colorbar
+clb = plt.colorbar(p,
+                   ax=ax,
+                   pad=0.08,
+                   shrink=0.85,
+                   aspect=12,
+                   ticks=levels,
+                   extendrect=True,
+                   extendfrac='auto',
+                   orientation='horizontal')
+
+# Set colorbar ticks
+clb.ax.xaxis.set_tick_params(length=0, labelsize=14, pad=9)
 
 # Use geocat.viz.util convenience function to add left and right titles
 gvutil.set_titles_and_labels(ax,
                              lefttitle="Surface temperature",
-                             righttitle="K")
+                             righttitle="K",
+                             lefttitlefontsize=22,
+                             righttitlefontsize=22)
 
 # Show the plot
+plt.tight_layout()
 plt.show()
