ea3b161b2f979a6e385cecca78a630f64df87d15
Jiaqi Li <jiaqili1620@gmail.com>
2021-07-14 15:25:08 -0600

    use inset axes

--- Plots/Overlays/NCL_overlay_12.py
+++ Plots/Overlays/NCL_overlay_12.py
@@ -4,12 +4,12 @@ NCL_overlay_12.py
 This script illustrates the following concepts:
    - Overlaying WRF "dbz" on a topographic map
    - Using two different colormaps on one page
-   - Using cnFillPalette to assign a color palette to contours
-   - Using opacity to emphasize or subdue overlain features
+   - Drawing a custom color bar using matplotlib.cm.ScalarMappable
+   - Using alpha argument to emphasize or subdue overlain features
    - Overlay multiple contours
    - Drawing counties in the United States
-   - Removing a plot that has been overlaid on another plot so it can be reused
-   - Controlling whether the labelbar shows same opacity as contours
+   - Using zorder to specify the order in which elements will be drawn
+   - Using inset_axes() to create additional axes for color bars
 
 See following URLs to see the reproduced NCL plot & script:
     - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/overlay_12.ncl
@@ -31,6 +31,7 @@ import geocat.datafiles as gdf
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from geocat.viz import util as gvutil
 from geocat.viz import cmaps as gvcmaps
 
@@ -69,15 +70,30 @@ print("{} ({}) : min={:.1f}    max={:.3f}".format(dbz.attrs['description'],
 ###############################################################################
 # Create plot
 
-#
-fig = plt.figure(figsize=(12, 8))
+# Generate figure (set its size (width, height) in inches)
+fig = plt.figure(figsize=(11.5, 10))
 
-#proj = get_cartopy(hgt)
 # Get Cartopy projection
 projection = ccrs.PlateCarree()
 
-# Set axes [left, bottom, width, height] to ensure map takes up entire figure
-ax = plt.axes([.05, -.05, .9, 1], projection=projection)
+# Set axes [left, bottom, width, height] to leave space for color bars and titles
+ax = plt.axes([0.05, 0.22, 0.9, 0.7], projection=projection)
+
+# Create inset axes for color bars
+cax1 = inset_axes(ax,
+                  width='98%',
+                  height='10%',
+                  loc='lower left',
+                  bbox_to_anchor=(0.01, -0.25, 1, 1),
+                  bbox_transform=ax.transAxes,
+                  borderpad=0)
+cax2 = inset_axes(ax,
+                  width='6%',
+                  height='98%',
+                  loc='lower right',
+                  bbox_to_anchor=(0.08, 0.01, 1, 1),
+                  bbox_transform=ax.transAxes,
+                  borderpad=0)
 
 # Set latitude and longitude extent to zoom in on map
 ax.set_extent([lon.min().data,
@@ -85,6 +101,9 @@ ax.set_extent([lon.min().data,
                lat.min().data,
                lat.max().data], projection)
 
+#
+# Add state and county borders
+#
 # Add US states borders
 ax.add_feature(cfeature.NaturalEarthFeature(category='cultural',
                                             name='admin_1_states_provinces',
@@ -104,66 +123,60 @@ ax.add_feature(COUNTIES,
                linewidth=0.2,
                zorder=5)
 
-# Set first contour levels
+#
+# Plot terrain height contour
+#
+# Import NCL color map
+cmap = gvcmaps.OceanLakeLandSnow
+# Set contour levels
 levels = np.array([0, 1] + [i for i in range(201, 3202, 200)])
-
-# Add first filled contour
+# Contourf hgt data
 contour = ax.contourf(lon,
                       lat,
                       hgt.data,
                       alpha=0.6,
                       levels=levels,
-                      cmap=gvcmaps.OceanLakeLandSnow,
+                      cmap=cmap,
                       zorder=3)
-
 # Add first color bar
 clb = fig.colorbar(contour,
+                   cax=cax1,
                    orientation='horizontal',
-                   shrink=0.7,
-                   pad=0.1,
-                   aspect=12,
                    ticks=levels[1:-1],
-                   anchor=(0.4, 0.8),
-                   drawedges=True,
-                   label="Terrain Height (m)")
-
+                   drawedges=True)
 # Manually set color bar tick length and pad
 clb.ax.xaxis.set_tick_params(length=0, pad=10)
+clb.set_label("Terrain Height (m)", fontsize=16, labelpad=-90)
 
-# Set second contourf levels
+#
+# Plot reflectivity contour
+#
+# Set contour levels
 levels = np.arange(-28, 41, 4)
-
-# Add second filled contour
+# Contourf dbz data
 contour2 = ax.contourf(lon,
                        lat,
                        dbz.data,
                        cmap='magma',
                        levels=levels,
                        zorder=4)
-
 # Set colormap and its bounds for the second contour
 cmap = plt.get_cmap('magma')
 colorbounds = np.arange(-30, 43, 2)
-
 # Use colormap to create a norm and mappable for colorbar to be correctly plotted
 norm = mcolors.BoundaryNorm(colorbounds, cmap.N)
 mappable = cm.ScalarMappable(norm=norm, cmap=cmap)
-
-# Add second color bar
-clb2 = fig.colorbar(mappable,
-                    pad=0.03,
-                    aspect=10,
-                    shrink=0.97,
-                    ticks=levels,
-                    drawedges=True)
-
+# Add color bar
+clb2 = fig.colorbar(mappable, cax=cax2, ticks=levels, drawedges=True)
 # Manually set color bar tick length and pad
 clb2.ax.yaxis.set_tick_params(length=0, pad=18, labelsize=14)
-
 # Center align colorbar tick labels
 ticklabs = clb2.ax.get_yticklabels()
 clb2.ax.set_yticklabels(ticklabs, ha='center')
 
+#
+# Set axes features (tick formats and main title)
+#
 # Use geocat.viz.util convenience function to add minor and major tick lines
 gvutil.set_axes_limits_and_ticks(ax,
                                  xticks=np.arange(-105, -84, 5),
@@ -176,16 +189,17 @@ gvutil.add_lat_lon_ticklabels(ax)
 gvutil.add_major_minor_ticks(ax,
                              y_minor_per_major=1,
                              x_minor_per_major=1,
-                             labelsize="xx-large")
+                             labelsize=16)
 
 # Set padding between tick labels and axes, and turn off ticks on top and right spines
-ax.tick_params(pad=14, top=False, right=False)
+ax.tick_params(pad=9, top=False, right=False)
 
 # Set title and title fontsize
 ax.set_title("Reflectivity ({}) at znu level = {:.3f}".format(
     dbz.attrs['units'], znu[1].data),
              fontweight='bold',
-             fontsize=26,
-             y=1.05)
+             fontsize=30,
+             y=1.03)
 
+# Show plot
 plt.show()
