83eab1ccf02fbe1d05629b597e47033d80167c5a
Jiaqi Li <jiaqili1620@gmail.com>
2021-07-14 09:49:43 -0600

    overlay contours

--- Plots/Overlays/NCL_overlay_12.py
+++ Plots/Overlays/NCL_overlay_12.py
@@ -6,7 +6,7 @@ This script illustrates the following concepts:
    - Using two different colormaps on one page
    - Using cnFillPalette to assign a color palette to contours
    - Using opacity to emphasize or subdue overlain features
-   - Using "overlay" to overlay multiple contours
+   - Overlay multiple contours
    - Drawing counties in the United States
    - Removing a plot that has been overlaid on another plot so it can be reused
    - Controlling whether the labelbar shows same opacity as contours
@@ -49,64 +49,98 @@ znu = getvar(wrfin, "ZNU")  # eta values
 dbz = getvar(wrfin, "dbz")[0, :, :]  # radar reflectivity in dBZ
 
 ###############################################################################
+# Debug information
+
+# This print output resembles NCL printMinMax function
+print("------------------------------------------")
+print("{} ({}) : min={:.1f}    max={:.3f}".format(hgt.attrs['description'],
+                                                  hgt.attrs['units'],
+                                                  hgt.min().data,
+                                                  hgt.max().data))
+print("{} ({}) : min={:.1f}    max={:.3f}".format(dbz.attrs['description'],
+                                                  dbz.attrs['units'],
+                                                  dbz.min().data,
+                                                  dbz.max().data))
+
+###############################################################################
 # Create plot
 
+#
 fig = plt.figure(figsize=(12, 8))
 
-projection = get_cartopy(hgt)
+#proj = get_cartopy(hgt)
+# Get Cartopy projection
+projection = ccrs.PlateCarree()
 
-# Add axes for lambert conformal map
+# Add axes with Cartopy projection
 ax = plt.axes(projection=projection)
 
-# Add state boundaries other lake features
-ax.add_feature(
-    cfeature.STATES,
-    edgecolor='black',
-    #linestyle=(0, (5, 10)),
-    zorder=4)
-
 # Set latitude and longitude extent of map
-#ax.set_extent([-119, -74, 18, 50], ccrs.Geodetic())
+ax.set_extent([lon.min().data,
+               lon.max().data,
+               lat.min().data,
+               lat.max().data], projection)
 
-# Set contour levels
+# Download the Natural Earth shapefile for country boundaries at 110m resolution
+# shapename = 'admin_1_states_provinces'
+# countries_shp = shpreader.natural_earth(resolution='10m',
+#                                         category='cultural',
+#                                         name=shapename)
+
+# # Add country borders
+# for state in shpreader.Reader(countries_shp).geometries():
+#     ax.add_geometries([state],
+#                       projection,
+#                       facecolor='none',
+#                       edgecolor='black',
+#                       zorder=5)
+
+# Add US states borders
+ax.add_feature(cfeature.NaturalEarthFeature(category='cultural',
+                                            name='admin_1_states_provinces',
+                                            scale='10m',
+                                            facecolor='none',
+                                            edgecolor='black',
+                                            linewidth=0.2),
+               zorder=5)
+
+# Set first contour levels
 levels = np.array([1] + [i for i in range(201, 3002, 200)])
 
 # Add filled contours
-hgt.plot.contourf(ax=ax,
+clb = ax.contourf(lon,
+                  lat,
+                  hgt.data,
                   cmap=gvcmaps.OceanLakeLandSnow,
                   levels=levels,
-                  transform=projection,
-                  add_label=False,
-                  add_colorbar=False)
+                  zorder=3)
 
-dbz.plot.contourf(ax=ax,
-                  cmap='magma',
-                  levels=levels,
-                  transform=projection,
-                  add_label=False,
-                  add_colorbar=False)
+fig.colorbar(clb, shrink=0.65, orientation='horizontal')
 
-# Set shape name of map (which depicts the United States)
-# shapename = 'admin_1_states_provinces_lakes'
-# states_shp = shpreader.natural_earth(resolution='110m',
-#                                       category='cultural',
-#                                       name=shapename)
+# Set second contourf levels for
+levels = np.arange(-28, 41, 4)
 
-# # # Add outlines of each state within the United States
-# for state in shpreader.Reader(states_shp).geometries():
-#     ax.add_geometries([state],
-#                       ccrs.PlateCarree(),
-#                       facecolor='white',
-#                       edgecolor='black',
-#                       zorder=2)
+clb2 = ax.contourf(lon, lat, dbz.data, cmap='magma', levels=levels, zorder=4)
+
+fig.colorbar(clb2)
+
+# Use geocat.viz.util convenience function to add minor and major tick lines
+gvutil.set_axes_limits_and_ticks(ax,
+                                 xticks=np.arange(-105, -84, 5),
+                                 yticks=np.arange(18, 35, 2))
+
+gvutil.add_lat_lon_ticklabels(ax)
+
+gvutil.add_major_minor_ticks(ax,
+                             y_minor_per_major=1,
+                             x_minor_per_major=1,
+                             labelsize="medium")
 
 # Set title and title fontsize of plot using gvutil function instead of matplotlib function call
 gvutil.set_titles_and_labels(
     ax,
-    maintitle="Reflectivity ({}) + at znu level = {:.3f}".format(
+    maintitle="Reflectivity ({}) at znu level = {:.3f}".format(
         dbz.attrs['units'], znu[1].data),
     maintitlefontsize=18)
-
-gvutil.add_lat_lon_ticklabels(ax)
 
 plt.show()
