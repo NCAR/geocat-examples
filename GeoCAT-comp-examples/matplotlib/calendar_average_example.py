0bf342ca7f4b15d181833d2bbbe2745b9db4a3be
Heather Craker <hcraker@ucar.edu>
2022-02-01 11:56:26 -0700

    Addressed inline comments from Orhan

--- GeoCAT-comp-examples/matplotlib/calendar_average_example.py
+++ GeoCAT-comp-examples/matplotlib/calendar_average_example.py
@@ -8,16 +8,16 @@ This script illustrates the following concepts:
 
 See following GitHub repositories to see further information about the function and how to access
 data:
-    - For GeoCAT functions: https://github.com/NCAR/geocat-comp
+    - For GeoCAT-comp functions: https://github.com/NCAR/geocat-comp
     - For atm.20C.hourly6-1990-1995.nc file: https://github.com/NCAR/geocat-datafiles/tree/main/netcdf_files
 
 Dependencies:
-    - cftime
     - geocat.comp
     - geocat.datafiles (Not necessary for figure but used for accessing the data file)
     - geocat.viz
-    - matplotlib
-    - xarray
+    - cftime (installed with geocat.comp)
+    - matplotlib (installed with geocat.viz)
+    - xarray (installed with geocat.comp)
 
 Figure Description:
 The top subplot is raw surface temperature data from a model run with a
