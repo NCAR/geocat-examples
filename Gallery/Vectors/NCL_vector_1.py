a6884d17e0f5621b26fcd4f1201389ddb1077a31
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-01 13:43:04 -0600

    Updating vector_1 example

--- Gallery/Vectors/NCL_vector_1.py
+++ Gallery/Vectors/NCL_vector_1.py
@@ -18,16 +18,17 @@ See following URLs to see the reproduced NCL plot & script:
 ###############################################################################
 # Import packages
 
-import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
-from matplotlib import pyplot as plt
-import cartopy
+import xarray as xr
+import cartopy.feature as cfeature
 import cartopy.crs as ccrs
-import cmaps
+from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 
 import geocat.datafiles as gdf
+import cmaps
 import geocat.viz as gv
-
 ###############################################################################
 # Read in data:
 
@@ -47,21 +48,43 @@ u = uv_in['U'].sel(date=198801, lev=1000)
 v = uv_in['V'].sel(date=198801, lev=1000)
 
 # Read in grid information
-lat_sst = sst['lat']
-lon_sst = sst['lon']
 lat_uv = u['lat']
 lon_uv = u['lon']
 
 ###############################################################################
 # Plot:
 
-# Generate figure (set its size (width, height) in inches)
-plt.subplots(figsize=(10, 7))
+# Define map projection to use
+proj = ccrs.PlateCarree()
 
-# Generate axes using Cartopy projection
-ax = plt.axes(projection=ccrs.PlateCarree())
+# Use utility function to import NCL colormap
+gv.truncate_colormap(cmaps.BlAqGrYeOrReVi200,
+                     minval=0.08,
+                     maxval=0.96,
+                     name="BlAqGrYeOrReVi200")
 
-# Draw vector plot
+# Define figure and axes
+plt.figure(figsize=(8, 5))
+ax = plt.axes(projection=proj)
+
+# add land feature and zoom in on desired location
+ax.add_feature(cfeature.LAND, facecolor="lightgrey", zorder=1)
+ax.set_extent((66, 96, 5, 25), crs=ccrs.PlateCarree())
+
+# Create the filled contour plot
+sst_plot = sst.plot.contourf(ax=ax,
+                             transform=proj,
+                             levels=50,
+                             vmin=24.0,
+                             vmax=28.9,
+                             cmap="BlAqGrYeOrReVi200",
+                             add_colorbar=False)
+
+# Remove default x and y labels from plot
+plt.xlabel("")
+plt.ylabel("")
+
+# Add vectors onto the plot
 Q = plt.quiver(lon_uv,
                lat_uv,
                u,
@@ -72,51 +95,59 @@ Q = plt.quiver(lon_uv,
                scale=75,
                zorder=2)
 
-# Turn on continent shading
-ax.add_feature(cartopy.feature.LAND,
-               edgecolor='lightgray',
-               facecolor='lightgray',
-               zorder=1)
+# Use geocat-viz utility function to format title
+gv.set_titles_and_labels(ax,
+                         maintitle='',
+                         maintitlefontsize=18,
+                         lefttitle="Sea Surface Temperature",
+                         lefttitlefontsize=18,
+                         righttitle="C",
+                         righttitlefontsize=18,
+                         xlabel=None,
+                         ylabel=None,
+                         labelfontsize=16)
 
-# Define levels for contour map (24, 24.1, ..., 28.8, 28.9)
-levels = np.linspace(24, 28.9, 50)
+# Format tick labels as latitude and longitudes
+gv.add_lat_lon_ticklabels(ax=ax)
 
-# Import an NCL colormap, truncating it by using geocat.viz.util convenience function
-gv.truncate_colormap(cmaps.BlAqGrYeOrReVi200,
-                     minval=0.08,
-                     maxval=0.96,
-                     n=len(levels),
-                     name='BlAqGrYeOrReVi200')
+# Use geocat-viz utility function to customize tick marks
+gv.set_axes_limits_and_ticks(ax,
+                             xlim=(65, 95),
+                             ylim=(5, 25),
+                             xticks=range(70, 95, 10),
+                             yticks=range(5, 27, 5))
 
-# Contourf-plot the SST data
-cf = sst.plot.contourf('lon',
-                       'lat',
-                       extend='both',
-                       levels=levels,
-                       cmap='BlAqGrYeOrReVi200',
-                       zorder=0,
-                       add_labels=False,
-                       add_colorbar=False)
+# Remove degree symbol from tick labels
+ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
+ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
 
-# Add color bar
+# Add minor tick marks
+gv.add_major_minor_ticks(ax,
+                         x_minor_per_major=4,
+                         y_minor_per_major=4,
+                         labelsize=14)
+
+# Add and customize colorbar
 cbar_ticks = np.arange(24, 29.1, .3)
-cbar = plt.colorbar(cf,
-                    orientation='vertical',
-                    drawedges=True,
-                    shrink=0.75,
-                    pad=0.05,
-                    ticks=cbar_ticks)
+plt.colorbar(ax=ax,
+             mappable=sst_plot,
+             extendrect=True,
+             shrink=0.88,
+             aspect=10,
+             ticks=cbar_ticks,
+             drawedges=True)
 
 # Draw the key for the quiver plot as a rectangle patch
-rect = plt.Rectangle((92.9, 22.6),
-                     2,
-                     2,
-                     facecolor='white',
-                     edgecolor=None,
-                     zorder=2)
+rect = mpl.patches.Rectangle((91.7, 22.7),
+                             3.1,
+                             2.2,
+                             facecolor='white',
+                             edgecolor='k',
+                             zorder=2)
 ax.add_patch(rect)
+
 ax.quiverkey(Q,
-             0.9675,
+             0.95,
              0.9,
              3,
              '4',
@@ -124,28 +155,8 @@ ax.quiverkey(Q,
              color='black',
              coordinates='axes',
              fontproperties={'size': 14},
-             labelsep=0.1)
-
-# Use geocat.viz.util convenience function to set axes tick values
-gv.set_axes_limits_and_ticks(ax,
-                             xlim=(65, 95),
-                             ylim=(5, 25),
-                             xticks=range(70, 95, 10),
-                             yticks=range(5, 27, 5))
-
-# Use geocat.viz.util convenience function to add minor and major tick lines
-gv.add_major_minor_ticks(ax,
-                         x_minor_per_major=5,
-                         y_minor_per_major=5,
-                         labelsize=14)
-
-# Use geocat.viz.util convenience function to make plots look like NCL plots by using latitude, longitude tick labels
-gv.add_lat_lon_ticklabels(ax)
-
-# Use geocat.viz.util convenience function to add titles to left and right of the plot axis.
-gv.set_titles_and_labels(ax,
-                         lefttitle='Sea Surface Temperature',
-                         righttitle='C')
+             labelsep=0.1,
+             zorder=3)
 
 # Show the plot
 plt.show()
