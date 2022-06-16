e22e00ef33bb2075686e32fc7c1e62293b8af5c9
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-16 16:42:50 -0600

    Plotting small topo dataset on top of cartopy map

--- Gallery/Topography/NCL_topo_1.py
+++ Gallery/Topography/NCL_topo_1.py
@@ -12,7 +12,43 @@ See following URLs to see the reproduced NCL plot & script:
 
 ###############################################################################
 # Import packages:
+import matplotlib.pyplot as plt
 import xarray as xr
+import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+import geocat.viz as gv
 
 ###############################################################################
 # Read in data:
+
+# Open a netCDF data file using xarray default engine and load the data into xarrays
+ds = xr.open_dataset('~/Downloads/colorado.nc')
+
+# Select Band2
+ds = ds.Band2
+
+###############################################################################
+# Plot
+
+# Generate figure and set size
+plt.figure(figsize=(10, 7))
+
+# Generate axes, using Cartopy
+projection = ccrs.PlateCarree()
+ax = plt.axes(projection=projection)
+
+ax.coastlines(zorder=10)
+ax.set_extent([-120, -92, 30,
+               50])  # Set map extent [lonmin, lonmax, latmin, latmax]
+
+states_provinces = cfeature.NaturalEarthFeature(
+    category='cultural',
+    name='admin_1_states_provinces_lines',
+    scale='50m',
+    facecolor='none')
+
+ax.add_feature(states_provinces, edgecolor='black')
+
+ds.plot(ax=ax, transform=projection, cmap='terrain', add_colorbar=False)
