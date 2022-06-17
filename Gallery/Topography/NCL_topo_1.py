bb418624a48c021addb74effb8f59715f86c3fdb
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-17 12:19:53 -0600

    Getting topo map working with formatting, using a smaller dataset

--- Gallery/Topography/NCL_topo_1.py
+++ Gallery/Topography/NCL_topo_1.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
-import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 
 import geocat.viz as gv
 
@@ -24,7 +24,7 @@ import geocat.viz as gv
 # Read in data:
 
 # Open a netCDF data file using xarray default engine and load the data into xarrays
-ds = xr.open_dataset('~/Downloads/colorado.nc')
+ds = xr.open_dataset('~/Downloads/nsamerica.nc')
 
 # Select Band2
 ds = ds.Band2
@@ -42,14 +42,24 @@ ax = plt.axes(projection=projection)
 ax.coastlines(zorder=10)
 
 # Set map extent [lonmin, lonmax, latmin, latmax]
-ax.set_extent([-120, -92, 30, 50])
+ax.set_extent([-120, -46, -5, 35])
 
-states_provinces = cfeature.NaturalEarthFeature(
-    category='cultural',
-    name='admin_1_states_provinces_lines',
-    scale='50m',
-    facecolor='none')
-
-ax.add_feature(states_provinces, edgecolor='black')
-
+# Pot the elevation data
 ds.plot(ax=ax, transform=projection, cmap='terrain', add_colorbar=False)
+
+# Use geocat-viz utility function to format major and minor tick marks
+gv.add_major_minor_ticks(ax, labelsize=12)
+
+# Use geocat-viz utility function to format title
+gv.set_titles_and_labels(ax, maintitle='ETOPO1', maintitlefontsize=20)
+
+# Use geocat-viz utility function to format x and y tick labels
+gv.set_axes_limits_and_ticks(ax,
+                             xticks=np.arange(-120, -45, 10),
+                             yticks=np.arange(-5, 45, 10))
+# Remove degree symbol from lat/lon labels
+ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
+ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
+
+# Use geocat-viz utility function to add lat/lon formatting for tick labels
+gv.add_lat_lon_ticklabels(ax)
