6046a48626d01e0c842c091421e7a00b7b19f5f4
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-22 13:14:39 -0600

    Adding formatting to plot

--- Gallery/Scatter/NCL_scatter_10.py
+++ Gallery/Scatter/NCL_scatter_10.py
@@ -18,20 +18,21 @@ See following URLs to see the reproduced NCL plot & script:
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 
 import geocat.viz as gv
 
 ###############################################################################
 # Create dummy data
 
-lat = np.arange(-75, 90, 20)  #np.random.uniform(-80, 80, 20)
-lon = np.arange(-170, 180, 20)  #np.random.uniform(-170, 170, 20)
+lat = np.arange(-70, 85, 20)
+lon = np.arange(-160, 170, 20)
 
 ###############################################################################
 # Plot
 
 # Generate a figure and axes
-plt.figure(figsize=(10, 9))
+plt.figure(figsize=(12, 10))
 
 projection = ccrs.PlateCarree()
 ax = plt.axes(projection=projection)
@@ -42,24 +43,37 @@ ax.coastlines()
 # Create an array of colors
 colors = ['blue', 'green', 'red', 'yellow', 'purple']
 
+# Plot random markers on the map
 for i in range(len(lat)):
     for j in range(len(lon)):
-        ax.scatter(lon[j],
-                   lat[i],
-                   color=np.random.choice(colors, size=1)[0],
-                   s=900,
-                   linewidths=1,
-                   alpha=0.75,
-                   zorder=2,
-                   marker='s')
+        # Generate a random number between 0 and 1
+        rand = np.random.random(size=1)
 
-# plt.xlim(-180, 190)
-# plt.ylim(-90, 90)
+        # Don't plot a marker if rand<0.35 (exit the loop)
+        if rand < 0.35:
+            continue
 
-gv.set_axes_limits_and_ticks(ax,
+        # Scatter plot
+        ax.scatter(
+            lon[j],  # Longitude of marker
+            lat[i],  # Latitude of marker
+            color=np.random.choice(colors,
+                                   size=1)[0],  # Randomly select a color
+            s=1230,  # Size of marker
+            alpha=0.75,  # Transparency of the marker
+            zorder=2,  # PLot markers on top of map
+            marker='s')  # Use a square shaped marker
+
+gv.add_lat_lon_ticklabels(ax)
+
+ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
+ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
+
+gv.add_major_minor_ticks(ax, labelsize=14)
+gv.set_axes_limits_and_ticks(ax=ax,
                              xlim=(-180, 190),
                              ylim=(-90, 100),
                              xticks=np.arange(-180, 190, 30),
                              yticks=np.arange(-90, 100, 30))
 
-gv.add_lat_lon_ticklabels(ax)
+plt.title('Dummy markers over a map', fontweight='bold', fontsize=20)
