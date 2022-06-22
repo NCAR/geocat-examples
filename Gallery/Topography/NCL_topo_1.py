f30e40695ec6c9150ebb8d80f908a48abf4c26dc
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-22 16:31:12 -0600

    Working global topo map

--- Gallery/Topography/NCL_topo_1.py
+++ Gallery/Topography/NCL_topo_1.py
@@ -17,6 +17,7 @@ import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
+import cmaps
 
 import geocat.viz as gv
 
@@ -29,10 +30,11 @@ import geocat.viz as gv
 # ee.Image("NOAA/NGDC/ETOPO1").wx.to_xarray()
 
 # Open a netCDF data file using xarray default engine and load the data into xarrays
-ds = xr.open_dataset('~/Downloads/nsamerica.nc')
-
+#ds = xr.open_dataset('~/Downloads/nsamerica.nc')
+ds = xr.open_dataset('/Users/dquint/Downloads/ETOPO1_Ice_g_gmt4.grd')
+ds = ds.z
 # Select Band2
-ds = ds.Band2
+#ds = ds.Band2
 
 ###############################################################################
 # Plot
@@ -47,10 +49,13 @@ ax = plt.axes(projection=projection)
 ax.coastlines(zorder=10)
 
 # Set map extent [lonmin, lonmax, latmin, latmax]
-ax.set_extent([-120, -46, -5, 30])
+#ax.set_extent([-120, -46, -5, 30])
 
 # Pot the elevation data
-elev = ds.plot(ax=ax, transform=projection, cmap='terrain', add_colorbar=False)
+elev = ds.plot(ax=ax,
+               transform=projection,
+               cmap=cmaps.GMT_relief,
+               add_colorbar=False)
 
 # Add colorbar
 plt.colorbar(ax=ax, mappable=elev, orientation='horizontal')
@@ -66,11 +71,11 @@ gv.set_titles_and_labels(ax,
                          ylabel="")
 
 # Use geocat-viz utility function to format x and y tick labels
-gv.set_axes_limits_and_ticks(ax,
-                             xlim=[-120, -46],
-                             ylim=[-5, 35],
-                             xticks=np.arange(-120, -45, 10),
-                             yticks=np.arange(-5, 38, 5))
+# gv.set_axes_limits_and_ticks(ax,
+#                              xlim=[-120, -46],
+#                              ylim=[-5, 35],
+#                              xticks=np.arange(-120, -45, 10),
+#                              yticks=np.arange(-5, 38, 5))
 
 # Use geocat-viz utility function to add lat/lon formatting for tick labels
 gv.add_lat_lon_ticklabels(ax)
@@ -79,20 +84,20 @@ gv.add_lat_lon_ticklabels(ax)
 ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
 ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
 
-import ee
+# import ee
 
-ee.Initialize()
+# ee.Initialize()
 
-import geemap
+# import geemap
 
-Map = geemap.Map(center=[40, -100])
+# Map = geemap.Map(center=[40, -100])
 
-dataset = ee.Image('NOAA/NGDC/ETOPO1')
-elevation = dataset.select('bedrock')
-elevationVis = {
-    'min': -7000.0,
-    'max': 3200.0,
-    'palette': ['#011de2', 'afafaf', '3603ff', 'fff477', 'b42109']
-}
-Map.setCenter(-37.62, 25.8, 2)
-Map.addLayer(elevation, elevationVis, 'Elevation')
+# dataset = ee.Image('NOAA/NGDC/ETOPO1')
+# elevation = dataset.select('bedrock')
+# elevationVis = {
+#     'min': -7000.0,
+#     'max': 3200.0,
+#     'palette': ['#011de2', 'afafaf', '3603ff', 'fff477', 'b42109']
+# }
+# Map.setCenter(-37.62, 25.8, 2)
+# Map.addLayer(elevation, elevationVis, 'Elevation')
