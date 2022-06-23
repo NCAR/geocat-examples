a0f7dc55f5789ff285b8ca96a4c96cf1f0ac31d0
Daphne Quint <99711690+doqhne@users.noreply.github.com>
2022-06-23 13:04:15 -0600

    Adding finished plot

--- Gallery/Scatter/NCL_scatter_10.py
+++ Gallery/Scatter/NCL_scatter_10.py
@@ -2,10 +2,9 @@
 NCL_scatter_10.py
 =================
 This script illustrates the following concepts:
-   - Drawing a scatter plot over a map using the "overlay" procedure
-   - Using gsn_csm_blank_plot to create a scatter plot with filled polygons
-   - Generating dummy data using "random_uniform"
-   - Changing the draw order of filled polygons
+   - Drawing a scatter plot over a map
+   - Customizing markers in a scatter plot
+   - Using numpy.random to generate random plot
 
 See following URLs to see the reproduced NCL plot & script:
     - Original NCL script: https://www.ncl.ucar.edu/Applications/Scripts/scatter_10.ncl
@@ -23,27 +22,26 @@ from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 import geocat.viz as gv
 
 ###############################################################################
-# Create dummy data
-
-lat = np.arange(-70, 85, 20)
-lon = np.arange(-160, 170, 20)
-
-###############################################################################
 # Plot
 
 # Generate a figure and axes
 plt.figure(figsize=(12, 10))
 
+# Define axis using Cartopy
 projection = ccrs.PlateCarree()
 ax = plt.axes(projection=projection)
 
 # Add coastlines to plot
 ax.coastlines()
 
+# Define latitude and longitudes for the markers
+lat = np.arange(-70, 85, 20)
+lon = np.arange(-160, 170, 20)
+
 # Create an array of colors
 colors = ['blue', 'green', 'red', 'yellow', 'purple']
 
-# Plot random markers on the map
+# Plot markers randomly on the map
 for i in range(len(lat)):
     for j in range(len(lon)):
         # Generate a random number between 0 and 1
@@ -59,21 +57,30 @@ for i in range(len(lat)):
             lat[i],  # Latitude of marker
             color=np.random.choice(colors,
                                    size=1)[0],  # Randomly select a color
-            s=1230,  # Size of marker
+            s=1296,  # Size of marker
             alpha=0.75,  # Transparency of the marker
-            zorder=2,  # PLot markers on top of map
+            zorder=2,  # Plot markers on top of map
             marker='s')  # Use a square shaped marker
 
+# Use geocat-viz utility function to format latitude/longitude labels
 gv.add_lat_lon_ticklabels(ax)
 
+# Remove degree symbol from tick labels
 ax.yaxis.set_major_formatter(LatitudeFormatter(degree_symbol=''))
 ax.xaxis.set_major_formatter(LongitudeFormatter(degree_symbol=''))
 
+# Use geocat-viz utility function to format major and minor tick marks
 gv.add_major_minor_ticks(ax, labelsize=14)
+
+# Use geocat-viz utility function to format tick labels
 gv.set_axes_limits_and_ticks(ax=ax,
-                             xlim=(-180, 190),
-                             ylim=(-90, 100),
+                             xlim=(-180, 181),
+                             ylim=(-90, 91),
                              xticks=np.arange(-180, 190, 30),
                              yticks=np.arange(-90, 100, 30))
 
-plt.title('Dummy markers over a map', fontweight='bold', fontsize=20)
+# Add title
+plt.title('Dummy markers over a map', fontweight='bold', fontsize=20, pad=20)
+
+# Show the plot
+plt.show()
