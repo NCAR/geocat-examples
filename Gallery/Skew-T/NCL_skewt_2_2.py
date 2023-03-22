9a7bb0b2754484b4f9257d218d4e225784d443c5
pilotchute <akootz@ucar.edu>
2023-03-22 12:06:20 -0600

    skewt fix so that tests will run
    Signed-off-by: pilotchute <akootz@ucar.edu>

--- Gallery/Skew-T/NCL_skewt_2_2.py
+++ Gallery/Skew-T/NCL_skewt_2_2.py
@@ -23,7 +23,7 @@ import metpy.calc as mpcalc
 
 import geocat.viz as gv
 import geocat.datafiles as gdf
-import geocat.comp.skewt_params as gcskewt
+from geocat.viz import get_skewt_vars
 
 ##############################################################################
 # Read in data:
@@ -48,7 +48,7 @@ u, v = mpcalc.wind_components(wspd, wdir)  # Calculate wind components
 tc0 = tc[0]  # Temperature of surface parcel
 tdc0 = tdc[0]  # Dew point temperature of surface parcel
 pro = mpcalc.parcel_profile(p, tc0, tdc0)  # Temperature profile of parcel
-subtitle = gcskewt.get_skewt_vars(p, tc, tdc, pro)  # Create subtitle
+subtitle = get_skewt_vars(p, tc, tdc, pro)  # Create subtitle
 
 ##############################################################################
 # Plot:
