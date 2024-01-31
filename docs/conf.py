c7b624c429f6a37e8f0c669d32cd5724afd836c2
Katelyn FitzGerald <7872563+kafitzgerald@users.noreply.github.com>
2024-01-30 19:40:05 -0700

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,6 @@ warnings.filterwarnings("ignore",
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
 html_theme = 'sphinx_book_theme'
 html_title = ""
 html_static_path = ['_static']
