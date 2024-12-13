08cb17bac64247b98852dc574971aad5265703d2
Katelyn FitzGerald <7872563+kafitzgerald@users.noreply.github.com>
2024-12-13 16:27:12 -0500

    Remaining changes to support Matplotlib 3.10 (#630)

--- Gallery/Contours/NCL_hov_3.py
+++ Gallery/Contours/NCL_hov_3.py
@@ -67,14 +67,7 @@ cs = ax.contour(lon,
                 levels=np.arange(-6, 12, 2),
                 colors='black',
                 linestyles="-",
-                linewidths=.2)
-
-# Set 0 level contour line to a thicker linewidth
-# If you try to access the "levels" attribute of cs (cs.levels),
-# the list of levels is: [-6, -4, -2, 0, 2, 4, 6, 8, 10]
-# level 0 is at the 3rd index of that list, so those contour lines
-# can be accessed at cs.collections[3]
-cs.collections[3].set_linewidth(1)
+                linewidths=[.2, .2, .2, 1, .2, .2, .2, .2, .2])
 
 # Label the contour levels -4, 0, and 4
 cl = ax.clabel(cs, fmt='%d', levels=[-4, 0, 4])
