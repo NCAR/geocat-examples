97d2e9202d17cee020ee3a4d690e18c2abf51be1
anissa111 <anissaz@ucar.edu>
2025-08-21 11:13:40 -0600

    update CF (thanks @kafitzgerald)

--- Gallery/Contours/NCL_trans_1.py
+++ Gallery/Contours/NCL_trans_1.py
@@ -17,7 +17,9 @@ See following URLs to see the reproduced NCL plot & script:
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import xarray as xr
+import numpy as np
 from pyproj import Geod
+from metpy.interpolate import cross_section
 
 import geocat.datafiles as gdf
 from geocat.comp import interp_multidim
@@ -29,9 +31,22 @@ from geocat.comp import interp_multidim
 # xarrays
 ds = xr.open_dataset(gdf.get("netcdf_files/h_avg_Y0191_D000.00.nc"), decode_times=False)
 
+# Add attrs for CF compliance to work with metpy package
+ds.time_bound.attrs['units'] = 'days since 0000-01-01 00:00:00'
+ds.time_bound.attrs['calendar'] = 'noleap'
+ds.time.attrs['calendar'] = 'noleap'
+
+ds = xr.decode_cf(ds)
+
+# format for metpy
+ds = ds.metpy.assign_crs(
+    grid_mapping_name='latitude_longitude',
+    earth_radius=6371229.0
+).rename({"lat_t": "lat", "lon_t": "lon"})
+
+
 # Pull out temperature
 t = ds.T[0,:,:,:]
-
 ##############################################################################
 # Calculate the great circle transect
 
@@ -54,9 +69,9 @@ gcdist = gcdist/1000    # convert to km
 lat_out = [p[0] for p in newpts]
 lon_out = [p[1] for p in newpts]
 
-transect = interp_multidim(t, lat_out, lon_out)
-
+transect = cross_section(t, (leftlat, leftlon), (rightlat, rightlon))
 ##############################################################################
 # Plot
 
 fig, ax = plt.subplots(1, 1)
+ax.contour(transect)
